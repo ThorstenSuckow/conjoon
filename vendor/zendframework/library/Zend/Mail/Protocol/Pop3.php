21dd81eed5f5b31673916aa5070f939c0a6a8bec
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2008-06-22 20:55:38 +0000

    - fixed: Zend_Mail_Protocol_Pop3 would not parse termination octets in multiline messages properly (see ZF-3479 http://framework.zend.com/issues/browse/ZF-3479)

--- vendor/zendframework/library/Zend/Mail/Protocol/Pop3.php
+++ vendor/zendframework/library/Zend/Mail/Protocol/Pop3.php
@@ -11,7 +11,7 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
- * 
+ *
  * @category   Zend
  * @package    Zend_Mail
  * @subpackage Protocol
@@ -185,7 +185,7 @@ class Zend_Mail_Protocol_Pop3
         if ($multiline) {
             $message = '';
             $line = fgets($this->_socket);
-            while ($line && trim($line) != '.') {
+            while ($line && $line != ".\r\n") {
                 $message .= $line;
                 $line = fgets($this->_socket);
             };
