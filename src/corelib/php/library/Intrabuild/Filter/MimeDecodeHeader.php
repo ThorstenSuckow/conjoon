759e764b20c98420739f16e486bc4c658cc96f69
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2008-07-28 02:28:58 +0000

    - fixed: some headers would not be correctly be encoded if the span over multiple lines and if quoted-printable encoded; adjusted filter-method to take multiple line header values into account

--- src/corelib/php/library/Intrabuild/Filter/MimeDecodeHeader.php
+++ src/corelib/php/library/Intrabuild/Filter/MimeDecodeHeader.php
@@ -6,11 +6,11 @@
  *
  * $Author$
  * $Id$
- * $Date$ 
+ * $Date$
  * $Revision$
  * $LastChangedDate$
  * $LastChangedBy$
- * $URL$ 
+ * $URL$
  */
 
 /**
@@ -25,13 +25,22 @@ require_once 'Zend/Filter/Interface.php';
 /**
  * iconv* is used internally by Zend_Mail for decoding, such as decoding header-parts
  * which are available in a format compliant to RFC2047 (@link http://www.faqs.org/rfcs/rfc2047).
- * 
+ *
  * There might be cases when a mail does submit the encoded headers lowercased, i.e.
  * instead of upper-case literals for hexadecimal values, they are lowercased.
  * iconv_mime_decode_mimeheader can for example not decode he following string:
  * =?UTF-8?Q?Ihr_pers=c3=b6nlicher_Xing-Newsletter_vom_19.05.2008?=
  * since the hexadecimal values are lower- instead of uppercased.
- * This filter replaces all ocuurences from =a..=f with their uppercase representative.
+ * This filter replaces all ocuurences from =a[...]..=f[...] with their uppercase
+ * representative.
+ * Some mail clients might also break the line right in a decoded char, e.g.
+ * "=?UTF-8?Q?[GUI_0000741]:_OBELIXgui2_-_l=C3=A4sst_sich_im_IE_7.0_nicht_=C3?=\r\n=?UTF-8?Q?=B6ffnen?="
+ * Notice the line break right in "=C3\r\n?UTF-8?Q?=B6", which is not allowed
+ * since its a decoded german umlaut ("ö"). The filter will also take this disallowed
+ * breaks into account and tries to fix them.
+ * It will also take header values into account which are separated by white spaces.
+ *
+ *
  *
  * @category   Filter
  * @package    Intrabuild_Filter
@@ -50,41 +59,78 @@ class Intrabuild_Filter_MimeDecodeHeader implements Zend_Filter_Interface
      */
     public function filter($value)
     {
-        if (strpos($value, '?Q?') === false && 
-            strpos($value, '?B?') === false) {
-            return $value;    
-        }
-        
-        $ms = explode("\r\n", $value);
-        if (count($ms) == 1) {
-            $ms = explode("\r", $value);
-            if (count($ms) == 1) {
-                $ms = explode("\n", $value);
+        $bid = '?q?';
+        $q = stripos($value, '?q?');
+        if ($q === false) {
+            $q = stripos($value, '?b?');
+            if ($q === false) {
+                return $value;
             }
+            $bid = '?b?';
         }
-        
-        for ($i = 0, $len = count($ms); $i < $len; $i++) {
-            $ms[$i] = trim($ms[$i]);
+
+        $ms = array(trim($value));
+
+        if (strpos($value, "\r\n") !== false) {
+            $ms = explode("\r\n", $value);
+        } else if (strpos($value, "\r") !== false) {
+            $ms = explode("\r", $value);
+        } else if (strpos($value, "\n") !== false) {
+            $ms = explode("\n", $value);
+        } else if (strpos(trim($value), " ") !== false) {
+            $ms = explode(" ", trim($value));
         }
-        
-        $ms = implode("", $ms);
-        
-        $s = str_replace(
-            array(
-                '=a', '=b', '=c', '=d', '=e', '=f', 
-            ) , 
-            array(
-                '=A', '=B', '=C', '=D', '=E', '=F', 
-            ), 
+
+        $len = count($ms);
+
+        $spec = "";
+
+        if ($len > 1) {
+
+            $index = -1;
+            for ($i = 0; $i < $len; $i++) {
+                $ms[$i] = trim($ms[$i]);
+                if (stripos($ms[$i], $bid) !== false) {
+                    if (strpos($ms[$i], '=') !== 0) {
+                        $spec = ' ';
+                    }
+                    break;
+                }
+                $index = $i;
+            }
+
+            if ($index != -1) {
+                $spec = implode(" ", array_slice($ms, 0, $index+1)) . $spec;
+                $ms   = array_slice($ms, $index+1);
+            }
+
+            $ms[0] = trim(substr($ms[0], 0, -2));
+            $chId  = substr($ms[0], 0, $q+3);
+            for ($i = 1, $len_i = count($ms); $i < $len_i; $i++) {
+                $ms[$i] = trim($ms[$i]);
+                if ($ms[$i] == "") {
+                    continue;
+                }
+                $ms[$i] = str_replace($chId, "", $ms[$i]);
+                $ms[$i] = substr($ms[$i], 0, -2);
+            }
+            $ms = implode("", $ms) . '?=';
+        } else {
+            $ms = implode("", $ms);
+        }
+
+        $s = preg_replace(
+            "/=(\d|[a-f]{1,}|[A-F]{1,})/e",
+            "strtoupper('\\0')",
             $ms
         );
-        
+
         $ret = @iconv_mime_decode_headers('A: '.$s);
-        
+
         if (!is_array($ret) || (is_array($ret) && !isset($ret['A']))) {
-            return $value;    
+            return $value;
         }
-       
-        return $ret['A'];
+
+        return $spec . $ret['A'];
     }
 }
