c07f016c240eff94085b624294d1ffdc7c4e8bc5
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2008-08-21 22:24:54 +0000

    - fixed: application would sometime throw the error that the response object was null, thus resulting in an error; added check to see if a fully configured object was passed to the loadRecords-method

--- src/corelib/js/source/groupware/email/EmailGrid.js
+++ src/corelib/js/source/groupware/email/EmailGrid.js
@@ -5,22 +5,22 @@
  *
  * $Author$
  * $Id$
- * $Date$ 
+ * $Date$
  * $Revision$
  * $LastChangedDate$
  * $LastChangedBy$
- * $URL$ 
+ * $URL$
  */
- 
+
 Ext.namespace('de.intrabuild.groupware.email');
 
 de.intrabuild.groupware.email.EmailGrid = function(config, controller) {
-    
+
     Ext.apply(this, config);
 
     this.controller = controller;
     this.enableHdMenu = false;
-    
+
 // ------------------------- set up buffered grid ------------------------------
 
     var reader = new Ext.ux.data.BufferedJsonReader({
@@ -29,38 +29,38 @@ de.intrabuild.groupware.email.EmailGrid = function(config, controller) {
 		versionProperty : 'version',
 		id              : 'id'
 	}, de.intrabuild.groupware.email.EmailItemRecord);
-	
+
     reader.read = function(response){
         var json = response.responseText;
         var o = eval("("+json+")");
         if(!o) {
             throw {message: "JsonReader.read: Json object not found"};
         }
-		
+
 		var z = this.readRecords(o);
 		if (o && (o.pendingItems === 0 || o.pendingItems > 0)) {
-            z.pendingItems = parseInt(o.pendingItems);       	
+            z.pendingItems = parseInt(o.pendingItems);
 		}
-		
+
 		return z;
-    };	
-    
+    };
+
 	// we receive the pendingItems count through a property
 	// in the response object. We store it in the options
 	var MyStore = Ext.extend(Ext.ux.grid.BufferedStore, {
-        
+
 		loadRecords: function(o, options, success)
 		{
-			if (o.pendingItems === 0 || o.pendingItems > 0) {
-                this.pendingItems = o.pendingItems; 
+			if (o && (o.pendingItems === 0 || o.pendingItems > 0)) {
+                this.pendingItems = o.pendingItems;
 			} else {
 				this.pendingItems = -1;
 			}
-			
+
 			MyStore.superclass.loadRecords.call(this, o, options, success);
 		}
 	});
-	
+
     this.store = new MyStore({
 		storeId     : Ext.id(),
         bufferSize  : 300,
@@ -68,11 +68,11 @@ de.intrabuild.groupware.email.EmailGrid = function(config, controller) {
         reader      : reader,
         sortInfo   : {field: 'date', direction: 'DESC'},
         pruneModifiedRecords : true,
-        remoteSort  : true,              
+        remoteSort  : true,
         url         : '/groupware/email/get.email.items/format/json'
     });
-	
-    
+
+
     this.view = new Ext.ux.grid.BufferedGridView({
         nearLimit   : 100,
         scrollDelay : 0,
@@ -87,30 +87,30 @@ de.intrabuild.groupware.email.EmailGrid = function(config, controller) {
             }
         }
     });
-    
-    this.selModel = new Ext.ux.grid.BufferedRowSelectionModel(); 
-    
+
+    this.selModel = new Ext.ux.grid.BufferedRowSelectionModel();
+
     /**
-     * You can use an instance of BufferedGridToolbar for keeping track of the 
+     * You can use an instance of BufferedGridToolbar for keeping track of the
      * current scroll position. It also gives you a refresh button and a loading
      * image that gets activated when the store buffers.
      * ...Yeah, I pretty much stole this one from the PagingToolbar!
-     */         
+     */
     this.bbar = new Ext.ux.BufferedGridToolbar({
         view        : this.view,
         displayInfo : true,
         displayMsg  : de.intrabuild.Gettext.gettext("Emails {0} - {1} of {2}"),
         emptyMsg    : de.intrabuild.Gettext.gettext("No emails available")
-    });      
-    
-// ------------------------- ^^ EO set up buffered grid ------------------------   
-    
+    });
+
+// ------------------------- ^^ EO set up buffered grid ------------------------
+
     this.columns = [{
         header: '<div class="de-intrabuild-groupware-email-EmailGrid-backgroundContainer '
-                + 'de-intrabuild-groupware-email-EmailGrid-attachmentColumn-headerBackground">&#160;</div>', 
-        width: 25, 
+                + 'de-intrabuild-groupware-email-EmailGrid-attachmentColumn-headerBackground">&#160;</div>',
+        width: 25,
         //align:'center',
-        sortable: true, 
+        sortable: true,
         resizable : false,
         dataIndex: 'isAttachment',
         renderer : function(value, p, record){
@@ -122,10 +122,10 @@ de.intrabuild.groupware.email.EmailGrid = function(config, controller) {
            }
       },{
         header : '<div class="de-intrabuild-groupware-email-EmailGrid-backgroundContainer '
-                 + 'de-intrabuild-groupware-email-EmailGrid-readColumn-headerBackground">&#160;</div>', 
-        width: 25, 
+                 + 'de-intrabuild-groupware-email-EmailGrid-readColumn-headerBackground">&#160;</div>',
+        width: 25,
         //align:'center',
-        sortable: true, 
+        sortable: true,
         resizable : false,
         dataIndex: 'isRead',
         renderer : function(value, p, record){
@@ -135,39 +135,39 @@ de.intrabuild.groupware.email.EmailGrid = function(config, controller) {
                              + '">&#160;</div>';
                    }
       },{
-        header: de.intrabuild.Gettext.gettext("Subject"), 
+        header: de.intrabuild.Gettext.gettext("Subject"),
         width: 415,
-        sortable: true, 
+        sortable: true,
         dataIndex: 'subject'
       },{
-        header: de.intrabuild.Gettext.gettext("From"), 
+        header: de.intrabuild.Gettext.gettext("From"),
         width: 220,
-        sortable: true, 
+        sortable: true,
         dataIndex: 'from'
-      },{    
+      },{
         header : '<div class="de-intrabuild-groupware-email-EmailGrid-backgroundContainer '
-                 + 'de-intrabuild-groupware-email-EmailGrid-spamColumn-headerBackground">&#160;</div>', 
-        width: 25, 
+                 + 'de-intrabuild-groupware-email-EmailGrid-spamColumn-headerBackground">&#160;</div>',
+        width: 25,
         //align:'center',
         resizable : false,
-        sortable: true, 
+        sortable: true,
         dataIndex: 'isSpam',
         renderer : function(value, p, record){
                        return '<div class="de-intrabuild-groupware-email-EmailGrid-backgroundContainer '
                                  + 'de-intrabuild-groupware-email-EmailGrid-spamColumn-cellBackground '
                                  + (value ? 'itemspam' : 'itemnospam')
                                  + '">&#160;</div>';
-           }        
+           }
       },{
-        header: de.intrabuild.Gettext.gettext("Date"), 
-        width: 120, 
+        header: de.intrabuild.Gettext.gettext("Date"),
+        width: 120,
         align:'right',
-        sortable: true, 
+        sortable: true,
         dataIndex: 'date',
         renderer: Ext.util.Format.dateRenderer('d.m.Y H:i')
       }
     ];
-    
+
 
     de.intrabuild.groupware.email.EmailGrid.superclass.constructor.call(this, {
         loadMask       : {msg: de.intrabuild.Gettext.gettext("Loading...")},
@@ -179,14 +179,14 @@ de.intrabuild.groupware.email.EmailGrid = function(config, controller) {
         ddText         : de.intrabuild.Gettext.gettext("{0} selected email(s)")
     });
 
-    this.on('contextmenu',    this.onContextClick, this);  
+    this.on('contextmenu',    this.onContextClick, this);
     this.on('rowcontextmenu', this.onRowContextClick, this);
-    
+
     this.store.on('beforeselectionsload', this.onBeforeSelectionsLoad, this);
     this.store.on('selectionsload',       this.onSelectionsLoad,       this);
-   
+
     this.store.proxy.on('loadexception', this.onLoadException, this);
-    
+
 };
 
 Ext.extend(de.intrabuild.groupware.email.EmailGrid, Ext.grid.GridPanel, {
@@ -200,88 +200,88 @@ Ext.extend(de.intrabuild.groupware.email.EmailGrid, Ext.grid.GridPanel, {
                 },
                 scope : this
             }
-        }); 
+        });
 	},
 
     onBeforeSelectionsLoad : function()
     {
         this.createContextMenu();
-     
+
         this.menu.items.get(9).setDisabled(true);
-        this.menu.items.get(9).setIconClass('de-intrabuild-groupware-selectionsLoading');   
-        
+        this.menu.items.get(9).setIconClass('de-intrabuild-groupware-selectionsLoading');
+
         var subItems  = this.menu.items.get(7).menu.items;
         subItems.get(0).setDisabled(true);
-        subItems.get(0).setIconClass('de-intrabuild-groupware-selectionsLoading');   
+        subItems.get(0).setIconClass('de-intrabuild-groupware-selectionsLoading');
         subItems.get(1).setDisabled(true);
-        subItems.get(1).setIconClass('de-intrabuild-groupware-selectionsLoading');   
+        subItems.get(1).setIconClass('de-intrabuild-groupware-selectionsLoading');
         subItems.get(3).setDisabled(true);
-        subItems.get(3).setIconClass('de-intrabuild-groupware-selectionsLoading');   
+        subItems.get(3).setIconClass('de-intrabuild-groupware-selectionsLoading');
         subItems.get(4).setDisabled(true);
-        subItems.get(4).setIconClass('de-intrabuild-groupware-selectionsLoading');   
-        
+        subItems.get(4).setIconClass('de-intrabuild-groupware-selectionsLoading');
+
     },
-    
+
     onSelectionsLoad : function()
     {
         this.createContextMenu();
-        
+
         this.menu.items.get(9).setDisabled(false);
-        this.menu.items.get(9).setIconClass(''); 
-        
+        this.menu.items.get(9).setIconClass('');
+
         var subItems  = this.menu.items.get(7).menu.items;
         subItems.get(0).setDisabled(false);
-        subItems.get(0).setIconClass('');   
+        subItems.get(0).setIconClass('');
         subItems.get(1).setDisabled(false);
-        subItems.get(1).setIconClass('');   
-        subItems.get(3).setIconClass('');   
-        subItems.get(4).setIconClass('');   
-        
+        subItems.get(1).setIconClass('');
+        subItems.get(3).setIconClass('');
+        subItems.get(4).setIconClass('');
+
         if (this.controller.clkNodeId == this.controller.treePanel.folderSpam.id) {
             subItems.get(3).setDisabled(true);
-            subItems.get(4).setDisabled(false); 
+            subItems.get(4).setDisabled(false);
         } else {
             subItems.get(3).setDisabled(false);
-            subItems.get(4).setDisabled(false); 
+            subItems.get(4).setDisabled(false);
         }
     },
-    
-    
+
+
     onContextClick : function(e)
     {
         e.stopEvent();
     },
-    
+
     onRowContextClick : function(grid, index, e)
     {
         var selModel = this.selModel;
-        
+
         var nodeType = this.controller.treePanel.getSelectionModel().getSelectedNode();
         if (nodeType) {
             nodeType = nodeType.attributes.type;
         }
-        
+
         this.createContextMenu();
-     
+
         e.stopEvent();
-        
+
         if (!selModel.isSelected(index)) {
             selModel.selectRow(index, false);
         }
-        
+
         var menuItems = this.menu.items;
         var subItems  = menuItems.get(7).menu.items;
-        
+
         // mark as spam has to be disabled in outbox, drafts and sent
         var tp = this.controller.treePanel;
         var clkNodeId = this.controller.clkNodeId;
         var isDrafts = clkNodeId == tp.folderDraft.id;
-        var disableSpamItems = isDrafts || clkNodeId == tp.folderOutbox.id  || 
+        var disableSpamItems = isDrafts || clkNodeId == tp.folderOutbox.id  ||
         						clkNodeId == tp.folderSent.id;
         var editDraft = menuItems.get(1);
         var openView = menuItems.get(0);
-        editDraft.setVisible(isDrafts);	
-        
+        editDraft.setVisible(isDrafts);
+
         if (selModel.getCount() == 1) {
             var ctxRecord = selModel.getSelected().data;
             openView.setDisabled(false);
@@ -295,25 +295,25 @@ Ext.extend(de.intrabuild.groupware.email.EmailGrid, Ext.grid.GridPanel, {
                 subItems.get(3).setDisabled((ctxRecord.isSpam == true) || disableSpamItems);
                 subItems.get(4).setDisabled((ctxRecord.isSpam == false) || disableSpamItems);
             }
-            
+
         } else {
         	openView.setDisabled(true);
         	editDraft.setDisabled(true);
             var pendingRanges = this.selModel.getPendingSelections(true);
-           
+
             if (pendingRanges.length == 0) {
                 this.onSelectionsLoad();
             } else {
                 this.store.loadSelections(pendingRanges);
             }
-        }        
-        
+        }
+
         this.menu.showAt(e.getXY());
-    },    
+    },
 
     createContextMenu : function()
     {
-        if(!this.menu){ 
+        if(!this.menu){
             this.menu = new Ext.menu.Menu({
                 items: [{
                 	text  : de.intrabuild.Gettext.gettext("Open in new tab"),
@@ -364,7 +364,7 @@ Ext.extend(de.intrabuild.groupware.email.EmailGrid, Ext.grid.GridPanel, {
                     handler : function(){this.controller.deleteEmails(this.selModel.getSelections());}
                 }]
             });
-        }        
+        }
     }
-    
+
 });
