61c6618a0dfab9568c33cedd0f70153b72f60a1d
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2008-09-19 20:19:08 +0000

    - enhancement: Added class for storing version information

---
+++ src/corelib/php/library/Intrabuild/Version.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * intrabuild
+ * (c) 2002-2008 siteartwork.de/MindPatterns
+ * license@siteartwork.de
+ *
+ * $Author: T. Suckow $
+ * $Id: Letterman.php 137 2008-09-18 21:43:27Z T. Suckow $
+ * $Date: 2008-09-18 23:43:27 +0200 (Do, 18 Sep 2008) $
+ * $Revision: 137 $
+ * $LastChangedDate: 2008-09-18 23:43:27 +0200 (Do, 18 Sep 2008) $
+ * $LastChangedBy: T. Suckow $
+ * $URL: file:///F:/svn_repository/intrabuild_rep/trunk/src/corelib/php/library/Intrabuild/Modules/Groupware/Email/Letterman.php $
+ */
+
+/**
+ * Class to store and retrieve the version of Intrabuild.
+ *
+ * @category   Intrabuild
+ * @package    Intrabuild_Version
+ */
+final class Intrabuild_Version
+{
+    /**
+     * Current version.
+     */
+    const VERSION = '0.1a';
+
+}
