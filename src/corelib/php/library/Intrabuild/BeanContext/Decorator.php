d0d916b597c6e87ebb2435ae9e2dd9a6df90349b
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2008-09-20 19:50:19 +0000

    - enhancement: added optional argument for not throwing an exception if a property is available for which no setter can be found

--- src/corelib/php/library/Intrabuild/BeanContext/Decorator.php
+++ src/corelib/php/library/Intrabuild/BeanContext/Decorator.php
@@ -6,19 +6,19 @@
  *
  * $Author$
  * $Id$
- * $Date$ 
+ * $Date$
  * $Revision$
  * $LastChangedDate$
  * $LastChangedBy$
- * $URL$ 
+ * $URL$
  */
 
-/** 
+/**
  * @see Intrabuild_BeanContext_Inspector
  */
 require_once 'Intrabuild/BeanContext/Inspector.php';
 
-/** 
+/**
  * @see Intrabuild_Util_Array
  */
 require_once 'Intrabuild/Util/Array.php';
@@ -33,7 +33,7 @@ require_once 'Intrabuild/Filter/Input.php';
  * to their applicable objects. If no susbstiute for the specified
  * method to decorate can be found, the method/property of the decorated
  * class will be used.
- * 
+ *
  * A ModelDecorator casts abstract storage data to the entities they represent.
  *
  * @package Intrabuild_BeanContext
@@ -41,8 +41,8 @@ require_once 'Intrabuild/Filter/Input.php';
  * @category BeanContext
  *
  * @author Thorsten Suckow-Homberg <ts@siteartwork.de>
- */    
-final class Intrabuild_BeanContext_Decorator {    
+ */
+final class Intrabuild_BeanContext_Decorator {
 
     const TYPE_DTO     = 'dto';
     const TYPE_ENTITY  = 'entity';
@@ -50,14 +50,14 @@ final class Intrabuild_BeanContext_Decorator {
     const TYPE_DEFAULT = 'default';
 
     /**
-     * @var Intrabuild_Filter_Input 
+     * @var Intrabuild_Filter_Input
      */
-    protected $_filter = null; 
+    protected $_filter = null;
 
     /**
-     * @var string 
+     * @var string
      */
-    protected $_entity = null; 
+    protected $_entity = null;
 
     /**
      * @var array
@@ -67,54 +67,63 @@ final class Intrabuild_BeanContext_Decorator {
     /**
      * @var Zend_Db_Table
      */
-    protected $_decoratedModel = null; 
+    protected $_decoratedModel = null;
+
+    /**
+     * @var boolean
+     */
+    protected $_strict = true;
 
     /**
      * Constructor
      *
      * @param Intrabuild_BeanContext_Decoratable $mixed  The table class to model, or the classname
-     * as a string of the model to decorate. The class will then try to 
+     * as a string of the model to decorate. The class will then try to
      * load the class and instantiate it with no arguments.
      * @param Intrabuild_Filter_Input $filter An optional filter that gets processed before the
      * data is returned. The flter will only be used if teh data is decorated as dto or entity
+     * @param boolean $strict if set to false, mising setters for any property available will not
+     * throw an exception
      *
      * @throws Exception
      */
-    final public function __construct($mixed, Intrabuild_Filter_Input $filter = null)
+    final public function __construct($mixed, Intrabuild_Filter_Input $filter = null, $strict = true)
     {
         $this->_filter = $filter;
-        
+
+        $this->_strict = $strict;
+
         if (is_string($mixed)) {
             Intrabuild_BeanContext_Inspector::loadClass($mixed);
             if (!class_exists($mixed)) {
                 throw new Exception("Could not find class to decorate \"$mixed\".");
-            }    
+            }
             $this->_decoratedModel = new $mixed;
         } else {
             $this->_decoratedModel = $mixed;
         }
-        
+
         if (!is_a($this->_decoratedModel, 'Intrabuild_BeanContext_Decoratable')) {
             throw new Exception("Decorated class is no subclass of \"Zend_Db_Table_Abstract\"");
         }
-        
+
         $this->_decoratedMethods = $this->_decoratedModel->getDecoratableMethods();
-        
+
         $this->_entity = $this->_decoratedModel->getRepresentedEntity();
-            
+
         Intrabuild_BeanContext_Inspector::loadClass($this->_entity);
         if (!class_exists($this->_entity)) {
             throw new Exception("Could not find entity \"".$this->_entity."\".");
-        } 
-        
+        }
+
         $refC = new ReflectionClass($this->_entity);
         if (!$refC->implementsInterface('Intrabuild_BeanContext')) {
             throw new Exception("Entity \"".$this->_entity."\" is not a \"Intrabuild_BeanContext\"-class.");
-        }       
+        }
     }
-     
+
     /**
-     * Method tries to determine if the called method was defined in 
+     * Method tries to determine if the called method was defined in
      * $_decoratedMethods. If this is the case, the return values will
      * be casted to instances of teh class defined in $_entity.
      * If the method called was not found in $_decorated, the method
@@ -124,7 +133,7 @@ final class Intrabuild_BeanContext_Decorator {
      * @param array $arguments The arguments passed.
      *
      * @return mixed
-     */ 
+     */
     final public function __call($method, $arguments)
     {
         $type = self::TYPE_DEFAULT;
@@ -146,7 +155,7 @@ final class Intrabuild_BeanContext_Decorator {
 
         if (in_array($method, $this->_decoratedMethods) && $type !== self::TYPE_DEFAULT) {
             $val = call_user_func_array(array($this->_decoratedModel, $method), $arguments);
-            return $this->_cast($val, $type);    
+            return $this->_cast($val, $type);
         } else {
             return call_user_func_array(array($this->_decoratedModel, $method), $arguments);
         }
@@ -165,59 +174,60 @@ final class Intrabuild_BeanContext_Decorator {
      */
     final protected function _cast($values, $type)
     {
-        if (is_a($values, 'Zend_Db_Table_Rowset_Abstract') || 
+        if (is_a($values, 'Zend_Db_Table_Rowset_Abstract') ||
             is_a($values, 'Zend_Db_Table_Row_Abstract') ||
             (is_object($values) && method_exists($values, 'toArray'))) {
-            $values = $values->toArray();    
+            $values = $values->toArray();
         }
-        
+
         if (!is_array($values) || $type == self::TYPE_ARRAY) {
-            return $values;    
+            return $values;
         }
-        
+
         $len = count($values);
         if ($len == 0) {
-            return array();    
+            return array();
         }
-        
+
         $entity = $this->_entity;
-        
+
         // simple check to determine if we have a assoc or numeric
         // array
         if (!Intrabuild_Util_Array::isAssociative($values)) {
             $data = array();
             for ($i = 0; $i < $len; $i++) {
                 Intrabuild_Util_Array::camelizeKeys($values[$i]);
-                
+
                 if ($this->_filter) {
                     $values[$i] = $this->_filter
                                        ->setData($values[$i])
-                                       ->getProcessedData();    
+                                       ->getProcessedData();
                 }
-                
+
                 $data[] = Intrabuild_BeanContext_Inspector::create(
                     $entity,
-                    $values[$i]
+                    $values[$i],
+                    !$this->_strict
                 );
                 if ($type == self::TYPE_DTO) {
-                    $data[$i] = $data[$i]->getDto();    
+                    $data[$i] = $data[$i]->getDto();
                 }
-            }    
+            }
             return $data;
         } else {
-            Intrabuild_Util_Array::camelizeKeys($values);  
+            Intrabuild_Util_Array::camelizeKeys($values);
             if ($this->_filter) {
                     $values = $this->_filter
                                    ->setData($values)
-                                   ->getProcessedData();    
+                                   ->getProcessedData();
             }
-            $data = Intrabuild_BeanContext_Inspector::create($entity, $values);
+            $data = Intrabuild_BeanContext_Inspector::create($entity, $values, !$this->_strict);
             if ($type == self::TYPE_DTO) {
-                return $data->getDto();    
+                return $data->getDto();
             }
             return $data;
         }
     }
-    
+
 
 }
--- src/corelib/php/library/Intrabuild/BeanContext/Inspector.php
+++ src/corelib/php/library/Intrabuild/BeanContext/Inspector.php
@@ -6,66 +6,66 @@
  *
  * $Author$
  * $Id$
- * $Date$ 
+ * $Date$
  * $Revision$
  * $LastChangedDate$
  * $LastChangedBy$
- * $URL$ 
+ * $URL$
  */
- 
+
 /**
  * Intrabuild_BeanContext
- */ 
+ */
 require_once 'Intrabuild/BeanContext.php';
 
 /**
  * Intrabuild_BeanContext_Exception
- */ 
+ */
 require_once 'Intrabuild/BeanContext/Exception.php';
- 
+
 /**
- * Static class with utility methods for working on classes implementing the 
+ * Static class with utility methods for working on classes implementing the
  * Intrabuild_BeanContext-interface and overall introspection of classes/object-instances.
  *
  * @category   Intrabuild
  * @package    Intrabuild_BeanContext
  *
  * @author Thorsten-Suckow-Homberg <ts@siteartwork.de>
- */ 
+ */
 class Intrabuild_BeanContext_Inspector {
-    
+
     /**
-     * @const string 
+     * @const string
      */
     const GETTER = 'get';
-    
+
     /**
-     * @const string 
-     */    
+     * @const string
+     */
     const SETTER = 'set';
-    
+
     /**
      * @array
      */
     private static $cache = array();
-    
+
     /**
      * Returns the reflection class for the object out of the cache.
      *
      * @param Object
      *
      * @return ReflectionClass
-     */    
+     */
     private static function getReflectionClass($object)
     {
         $class = get_class($object);
         if (!isset(self::$cache[$class]['class'])) {
             self::$cache[$class]['class'] = new ReflectionClass($object);
-        } 
-          
+        }
+
         return self::$cache[$class]['class'];
     }
-    
+
     /**
      * Returns reflection properties for the class the object represents
      * out of the cache.
@@ -76,16 +76,16 @@ class Intrabuild_BeanContext_Inspector {
      */
     private static function getReflectionProperties($object)
     {
-        $class = get_class($object);    
-        
+        $class = get_class($object);
+
         if (!isset(self::$cache[$class]['properties'])) {
             $refClass = self::getReflectionClass($object);
-            self::$cache[$class]['properties'] = $refClass->getProperties();    
-        } 
-        
+            self::$cache[$class]['properties'] = $refClass->getProperties();
+        }
+
         return self::$cache[$class]['properties'];
-    } 
-    
+    }
+
     /**
      * Returns reflection methods for the class the object represents
      * out of the cache.
@@ -93,52 +93,52 @@ class Intrabuild_BeanContext_Inspector {
      * @param Object
      *
      * @return Array|ReflectionMethod
-     */    
+     */
     private static function getReflectionMethods($object)
     {
-        $class = get_class($object);    
-        
+        $class = get_class($object);
+
         if (!isset(self::$cache[$class]['methods'])) {
             $refClass = self::getReflectionClass($object);
-            self::$cache[$class]['methods'] = $refClass->getMethods();    
-        } 
-        
+            self::$cache[$class]['methods'] = $refClass->getMethods();
+        }
+
         return self::$cache[$class]['methods'];
-    }     
-    
+    }
+
     /**
      * Checks wether a property is transient. A property is transient,
      * if an underscore is part of the property-name. This method
-     * will also check if a property with the same name is available 
+     * will also check if a property with the same name is available
      * that starts with an underscore.
      *
-     * @param string $propertyName 
+     * @param string $propertyName
      * @param Object $object
      *
-     * @return boolean true, if the property is transient, otherwise false 
+     * @return boolean true, if the property is transient, otherwise false
      */
     private static function isTransient($propertyName, $object)
     {
         if (strpos($propertyName, '_') !== false) {
-            return true;    
-        }    
-        
+            return true;
+        }
+
         $refClass = self::getReflectionClass($object);
-        
+
         try {
             $prop = $refClass->getProperty('_'.$propertyName);
             return true;
         } catch (ReflectionException $e) {
-            
+
         }
-        
+
         return false;
     }
-    
+
     /**
-     * Will return the properties as an associative array (key/value pairs) 
+     * Will return the properties as an associative array (key/value pairs)
      * of the specified object based on the setter-methods it implements along
-     * with the properties it defines. A property will only be returned if it 
+     * with the properties it defines. A property will only be returned if it
      * matches the definition for an <tt>Intrabuild_BeanContext</tt>-property, though
      * it must not neccessarily implement <tt>Intrabuild_BeanContext</tt>.
      *
@@ -155,89 +155,91 @@ class Intrabuild_BeanContext_Inspector {
     {
         $properties = self::getReflectionProperties($object);
         $methods    = self::getReflectionMethods($object);
-        
+
         $accessorType = self::GETTER;
-        
+
         $data = array();
-        
+
         /**
          * @todo better regex for properties containing only letters
          */
         for ($i = 0, $len = count($properties); $i < $len; $i++) {
             $name = $properties[$i]->getName();
             if (strpos($name, '_') !== false) {
-                continue; 
+                continue;
             }
-            
+
             $method = self::getAccessorForProperty($object, $name, $accessorType);
             if ($method === null) {
-                continue;    
-            }    
-          
-            try {  
-                $data[$name] = $object->$method(); 
-            } catch (Exception $e) {
-                throw new Intrabuild_BeanContext_Exception($e->getMessage());    
+                continue;
             }
-        } 
-        
+
+            try {
+                $data[$name] = $object->$method();
+            } catch (Exception $e) {
+                throw new Intrabuild_BeanContext_Exception($e->getMessage());
+            }
+        }
+
         return $data;
     }
 
     /**
      * Tries to instantiate a class based on the passed arguments.
-     * 
+     *
      * @param string $className The class that should be created. Must
      * implement the interface Intrabuild_BeanContext
      * @param array $properties An associative array with key/value-pairs,
      * representing the object properties to set. The Constructor will search for
-     * appropriate getter/setter methods and call them, passing the values as 
+     * appropriate getter/setter methods and call them, passing the values as
      * arguments.
-     * 
+     * @param boolean $ignoreMissingSetter true to not throw an error if a property
+     * occures for which no setter is available
+     *
      * @return Object
-     * @throws Intrabuild_BeanContext_Exception if any error during object 
+     * @throws Intrabuild_BeanContext_Exception if any error during object
      * instantiating or class loading occured
      */
-    public static function create($className, $properties)
+    public static function create($className, $properties, $ignoreMissingSetter = false)
     {
         self::loadClass($className);
-        
+
         if (!class_exists($className, false)) {
-            throw new Intrabuild_BeanContext_Exception("$className not found.");    
-        } 
-          
-        $object = new $className;    
-        
-        if (!($object instanceof Intrabuild_BeanContext)) {
-            throw new Intrabuild_BeanContext_Exception("$className does not implement Intrabuild_BeanContext.");        
+            throw new Intrabuild_BeanContext_Exception("$className not found.");
         }
-        
+
+        $object = new $className;
+
+        if (!($object instanceof Intrabuild_BeanContext)) {
+            throw new Intrabuild_BeanContext_Exception("$className does not implement Intrabuild_BeanContext.");
+        }
+
         try {
             foreach ($properties as $property => $value) {
                 /**
                  * @todo better regex for anything except letters
                  */
                 if (self::isTransient($property, $object) === false) {
-                    self::setProperty($object, $property, $value);    
+                    self::setProperty($object, $property, $value, $ignoreMissingSetter);
                 }
             }
         } catch (Exception $e) {
             if (($e instanceof Intrabuild_BeanContext_Exception)) {
-                throw $e;    
+                throw $e;
             } else {
-                throw new Intrabuild_BeanContext_Exception($e->getMessage() . '[originally thrown by '.get_class($e).']');    
+                throw new Intrabuild_BeanContext_Exception($e->getMessage() . '[originally thrown by '.get_class($e).']');
             }
         }
-        
+
         return $object;
     }
-    
+
     /**
      * Helper for getting a getter-method based on the property passed
      * as the argument.
      *
      * @param Object $object The object in which the accessor should be looked up
-     * @param string $propertyName The name of the property which accessor has to 
+     * @param string $propertyName The name of the property which accessor has to
      * be looked up
      * @param string $accessorType Which accessor-method to look up, either 'set'
      * or 'get'
@@ -248,43 +250,43 @@ class Intrabuild_BeanContext_Inspector {
     private static function getAccessorForProperty($object, $propertyName, $accessorType = self::GETTER)
     {
         $prefix = 'get';
-        
+
         switch ($accessorType) {
             case self::GETTER:
                 $prefix = 'get';
             break;
-            
+
             case self::SETTER:
                 $prefix = 'set';
-            break;  
-            
+            break;
+
             default:
-                return null;  
+                return null;
         }
-        
+
         $method = '';
 
         if ($accessorType === self::SETTER) {
             if (strpos($propertyName, 'is') === 0) {
-                $method = 'set'.substr($propertyName, 2); 
-            } else { 
+                $method = 'set'.substr($propertyName, 2);
+            } else {
                 $method = 'set'.ucfirst($propertyName);
             }
         } else if ($accessorType === self::GETTER) {
             if (strpos($propertyName, 'is') === 0) {
-                $method = 'is'.ucfirst($propertyName);        
+                $method = 'is'.ucfirst($propertyName);
             } else {
-                $method = 'get'.ucfirst($propertyName);    
+                $method = 'get'.ucfirst($propertyName);
             }
         }
-        
+
         if (method_exists($object, $method)) {
-            return $method;    
-        }        
-        
+            return $method;
+        }
+
         return null;
-    } 
-    
+    }
+
     /**
      * Helper for setting the properties of an Intrabuild_BeanContext-object.
      * Will throw an exception if the method associated with the property was
@@ -296,26 +298,30 @@ class Intrabuild_BeanContext_Inspector {
      * @param Object $object The instance to set the property for. Must be of type
      * <tt>Intrabuild_BeanContext</tt>
      * @param string $property The property name that is about to be set.
-     * @param mixed $value The value to pass as the argument to the 
+     * @param mixed $value The value to pass as the argument to the
      * setter-method of <tt>$property</tt>
+     * @param boolean $ignoreMissing when set to true, the method will not throw an error
+     * if the
      *
-     * @throws Exception 
+     * @throws Exception
      * @see Intrabuild_BeanContext
      */
-    private static function setProperty($object, $property, $value)
+    private static function setProperty($object, $property, $value, $ignoreMissing = false)
     {
         $method = self::getAccessorForProperty($object, $property, self::SETTER);
-        
-        if ($method === null) {
+
+        if ($method === null && $ignoreMissing == false) {
             throw new Intrabuild_BeanContext_Exception("Setter for $property not found.")  ;
+        } else if ($method === null) {
+            return;
         }
-        
-        $object->$method($value);    
-    } 
+
+        $object->$method($value);
+    }
 
     /**
      * Helper for loading a class.
-     * 
+     *
      */
     public static function loadClass($className)
     {
@@ -325,4 +331,4 @@ class Intrabuild_BeanContext_Inspector {
         }
     }
 
-} 
+}
