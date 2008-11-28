fce3196daff7796a57e97c1427e9a1669dd5836c
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2008-11-28 21:59:26 +0000

    - enhancement: minor code cleanups

--- src/www/application/modules/default/controllers/IndexController.php
+++ src/www/application/modules/default/controllers/IndexController.php
@@ -20,42 +20,6 @@ require_once 'Zend/Controller/Action.php';
 
 class IndexController extends Zend_Controller_Action {
 
-    public function phpinfoAction()
-    {
-        echo phpinfo();
-        die();
-    }
-
-    public function sandboxAction()
-    {
-        var_dump(md5(uniqid(rand(), true)));
-        die();
-
-        require_once 'Intrabuild/Filter/MimeDecodeHeader.php';
-
-        $filter = new Intrabuild_Filter_MimeDecodeHeader();
-
-        $subjects = array(
-            "=?UTF-8?Q?Xing:_Jennifer_Suckow_m=c3=b6cht?= =?UTF-8?Q?e_Sie_als_Kontakt_hinzuf=c3=bcgen?=",
-            "Xing: Steffen Hiller hat\r\n=?UTF-8?Q?Sie_als_Kontakt_best=c3=a4tigt?=",
-            "=?UTF-8?Q?[GUI_0000741]:_OBELIXgui2_-_l=C3=A4sst_sich_im_IE_7.0_nicht_=C3?=\r\n=?UTF-8?Q?=B6ffnen?=",
-            "=?utf-8?Q?iPersonic_News=3a_Mach_mehr_aus_deinem_Pers=c3=b6nlichkeitstyp=21?="
-        );
-
-
-        for ($i = 0, $len = count($subjects); $i < $len; $i++) {
-            $filtered = $filter->filter($subjects[$i]);
-            echo "<pre>";
-            var_dump($filtered);
-        }
-
-
-
-
-
-        die();
-    }
-
     public function indexAction()
     {
 
