8ea23b5602ad4f0e8dd32f538bfeeaf92abebb0b
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2009-03-29 09:55:55 +0000

    - enhancement: some minor code layout changes  - fixed : some labels were not wrapped using the gettext functionality

--- src/corelib/js/source/groupware/workbench/Menubar.js
+++ src/corelib/js/source/groupware/workbench/Menubar.js
@@ -34,49 +34,43 @@ com.conjoon.groupware.workbench.Menubar = function(){
 
                 _menubar = new Ext.Toolbar({
                     cls   : 'com-conjoon-groupware-Menubar',
-                    items : [
-{
-            text:'File',
-            menu: [{
-                text:'New',
-                menu : [com.conjoon.groupware.email.decorator.AccountActionComp.decorate(new Ext.menu.Item({
-                    text : 'Email...',
-                    handler : function() {
-                       com.conjoon.groupware.email.EmailEditorManager.createEditor();
-                    }
-                }))]
-            }, '-', {
-                text    : "Logout...",
-                handler : function() {
-                    com.conjoon.groupware.Reception.showLogout();
-                }
-            }]
-        },
-            {
-                text:'Extras',
-                menu: [{
-                      text : "Feeds...",
-                      handler : function() {
-                          var dialog = new com.conjoon.groupware.feeds.FeedOptionsDialog();
-                          dialog.show();
-                      }
-                      }, '-', {
-                      text:com.conjoon.Gettext.gettext("Email accounts..."),
-                      handler : function() {
-                        var dialog = new com.conjoon.groupware.email.EmailAccountDialog();
-                        dialog.show();
-                      }
-                     }]
-            },
-            {
-                text:'?',
-                menu: [{
-                      text:com.conjoon.Gettext.gettext("About")
-                     }]
-            }
-
-                    ]
-
+                    items : [{
+                        text : com.conjoon.Gettext.gettext("File"),
+                        menu : [{
+                            text : com.conjoon.Gettext.gettext("New"),
+                            menu : [com.conjoon.groupware.email.decorator.AccountActionComp.decorate(new Ext.menu.Item({
+                                text    : com.conjoon.Gettext.gettext("Email..."),
+                                handler : function() {
+                                   com.conjoon.groupware.email.EmailEditorManager.createEditor();
+                                }
+                            }))]
+                        }, '-', {
+                            text    : com.conjoon.Gettext.gettext("Logout..."),
+                            handler : function() {
+                                com.conjoon.groupware.Reception.showLogout();
+                            }
+                        }]
+                    }, {
+                        text : com.conjoon.Gettext.gettext("Extras"),
+                        menu : [{
+                              text    : com.conjoon.Gettext.gettext("Feeds..."),
+                              handler : function() {
+                                  var dialog = new com.conjoon.groupware.feeds.FeedOptionsDialog();
+                                  dialog.show();
+                              }
+                              }, '-', {
+                              text    : com.conjoon.Gettext.gettext("Email accounts..."),
+                              handler : function() {
+                                var dialog = new com.conjoon.groupware.email.EmailAccountDialog();
+                                dialog.show();
+                              }
+                             }]
+                    }, {
+                        text : com.conjoon.Gettext.gettext("?"),
+                        menu : [{
+                              text : com.conjoon.Gettext.gettext("About")
+                             }]
+                    }]
                 });
 
             }
