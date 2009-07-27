2f73333c52d3a324d38d406efe1c285a428e15e6
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2009-07-27 17:08:21 +0000

    - fixed: fixed the filter so that tagged urls (a-tag, img-tag) do not get processed again

--- src/corelib/php/library/Conjoon/Filter/UrlToATag.php
+++ src/corelib/php/library/Conjoon/Filter/UrlToATag.php
@@ -54,18 +54,53 @@ class Conjoon_Filter_UrlToATag implements Zend_Filter_Interface
      * Returns the text with all patterns that look like a hyperlink or an address
      * in the www replaced by a <a href="{pattern}">{pattern}</a>.
      *
+     *
      * @param  mixed $value
      * @return integer
      */
     public function filter($value)
     {
+        // dev note: the regex is a little bit clumsy: it first extracts all contents in between
+        // "a"-tags and "img"-tags and assigns a placeholder, the processes the regex for the urls
+        // and remaps the placeholders with their cached values
+        // RegEx experts, feel free to contribute a more elegant solution!
+
+        // this array maps all replaced elements
+        $replaceMap = array();
+        $mk          = time();
+
+        // replace all a tags
+        $pattern = "/(<(a)\b[^>]*>)+(.*?)(<\/\\2>)+/is";
+        $matches = array();
+        preg_match_all($pattern, $value, $matches, PREG_SET_ORDER);
+        for ($i = 0, $len = count($matches); $i < $len; $i++) {
+            $value = str_replace($matches[$i][0], '@a@'.$mk.'_'.$i.'@a@', $value);
+            $replaceMap[] = array($matches[$i][0], '@a@'.$mk.'_'.$i.'@a@');
+        }
+
+        // replace all img tags
+        $pattern = "/(<img\b[^>]*\/>)/is";
+        $matches = array();
+        preg_match_all($pattern, $value, $matches, PREG_SET_ORDER);
+        for ($i = 0, $len = count($matches); $i < $len; $i++) {
+            $value = str_replace($matches[$i][0], '@img@'.$mk.'_'.$i.'@img@', $value);
+            $replaceMap[] = array($matches[$i][0], '@img@'.$mk.'_'.$i.'@img@');
+        }
+
+        // now work on remaining links
+        $pattern = "/(?:(ftp:\/\/|https:\/\/|http:\/\/)|(www\.))([a-zA-Z0-9-:\.\/\_\?\%\#\&\=\;\~\!\(\)]+)/ie";
         $value = preg_replace(
-            //                   1                  2        3           4          5
-            "/(?:(ftp:\/\/|https:\/\/|http:\/\/)|(www\.))(\S+\b\/?)([ [:punct:]]*)(\s|$)/ie",
-            "'<a ".$this->_attributeString." href=\"'.('\\1' ? '\\1' : 'http://').'$2$3\">$1$2$3</a>$4$5'",
+            $pattern,
+            "'<a ".$this->_attributeString." href=\"'.('\\1' ? '\\1' : 'http://').'$2$3\">$1$2$3</a>'",
             $value
         );
 
+        // reassign replacements
+        for ($i = 0, $len = count($replaceMap); $i < $len; $i++) {
+            $value = str_replace($replaceMap[$i][1], $replaceMap[$i][0], $value);
+        }
+
         return $value;
     }
+
 }
