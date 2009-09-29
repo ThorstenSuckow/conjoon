324de2099dfe517a78ac12f1cc9148e1a2b1670a
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2009-09-29 17:19:17 +0000

    - fixed: updated code due to changes in Zend framework 1.9.3PL1

--- src/corelib/php/library/Conjoon/Config/Array.php
+++ src/corelib/php/library/Conjoon/Config/Array.php
@@ -34,112 +34,58 @@ require_once 'Zend/Config/Ini.php';
 class Conjoon_Config_Array extends Zend_Config_Ini {
 
     /**
-     * Constructor.
-     * Works exactly like Zend_Config_Ini, with the only difference that instead
-     * of a filename, a config array is oassed as the argument.
+     * Loads the section $section from the array $config for
+     * access facilitated by nested object properties.
      *
-     * Unfortunately, lots  of the logic for processing the array has been put
-     * into the constructor of Zend_Config_Ini, this is why the implementation
-     * is taken 1:1 from the parent's implementation.
-     * Anyway, all kudos for this code to the Zend Framework COntributors!
      *
-     * @param  array         $iniArray
+     * @param  array         $config
      * @param  string|null   $section
      * @param  boolean|array $options
      * @throws Zend_Config_Exception
      * @return void
      */
-    public function __construct(Array $iniArray, $section = null, $options = false)
+    public function __construct(Array $config, $section = null, $options = false)
     {
-        $allowModifications = false;
-        if (is_bool($options)) {
-            $allowModifications = $options;
-        } elseif (is_array($options)) {
-            if (isset($options['allowModifications'])) {
-                $allowModifications = (bool) $options['allowModifications'];
-            }
-            if (isset($options['nestSeparator'])) {
-                $this->_nestSeparator = (string) $options['nestSeparator'];
-            }
-        }
+        parent::__construct($config, $section, $options);
+    }
 
-        $preProcessedArray = array();
-        foreach ($iniArray as $key => $data) {
-            $bits = explode(':', $key);
-            $thisSection = trim($bits[0]);
-            switch (count($bits)) {
+    /**
+     * Overrides parent implementation by passing an already parsed ini array to this
+     * method.
+     *
+     * @param Array $config
+     * @throws Zend_Config_Exception
+     * @return array
+     */
+    protected function _loadIniFile(Array $config)
+    {
+        $loaded = $config;
+
+        $iniArray = array();
+        foreach ($loaded as $key => $data)
+        {
+            $pieces = explode($this->_sectionSeparator, $key);
+            $thisSection = trim($pieces[0]);
+            switch (count($pieces)) {
                 case 1:
-                    $preProcessedArray[$thisSection] = $data;
+                    $iniArray[$thisSection] = $data;
                     break;
 
                 case 2:
-                    $extendedSection = trim($bits[1]);
-                    $preProcessedArray[$thisSection] = array_merge(
-                        array(';extends'=>$extendedSection), $data
-                    );
+                    $extendedSection = trim($pieces[1]);
+                    $iniArray[$thisSection] = array_merge(array(';extends'=>$extendedSection), $data);
                     break;
 
                 default:
                     /**
-                     * @see Conjoon_Config_Exception
+                     * @see Zend_Config_Exception
                      */
-                    require_once 'Conjoon/Config/Exception.php';
-                    throw new Conjoon_Config_Exception(
-                        "Section '$thisSection' may not extend ".
-                        "multiple sections in $filename"
-                    );
+                    require_once 'Zend/Config/Exception.php';
+                    throw new Zend_Config_Exception("Section '$thisSection' may not extend multiple sections in $filename");
             }
         }
 
-        if (null === $section) {
-            $dataArray = array();
-            foreach ($preProcessedArray as $sectionName => $sectionData) {
-                if(!is_array($sectionData)) {
-                    $dataArray = array_merge_recursive(
-                        $dataArray,
-                        $this->_processKey(array(), $sectionName, $sectionData)
-                    );
-                } else {
-                    $dataArray[$sectionName] = $this->_processExtends(
-                        $preProcessedArray, $sectionName
-                    );
-                }
-            }
-            Zend_Config::__construct($dataArray, $allowModifications);
-        } elseif (is_array($section)) {
-            $dataArray = array();
-            foreach ($section as $sectionName) {
-                if (!isset($preProcessedArray[$sectionName])) {
-                    /**
-                     * @see Conjoon_Config_Exception
-                     */
-                    require_once 'Conjoon/Config/Exception.php';
-                    throw new Conjoon_Config_Exception(
-                        "Section '$sectionName' cannot be found in $filename"
-                    );
-                }
-                $dataArray = array_merge(
-                    $this->_processExtends($preProcessedArray, $sectionName), $dataArray
-                );
-
-            }
-            Zend_Config::__construct($dataArray, $allowModifications);
-        } else {
-            if (!isset($preProcessedArray[$section])) {
-                /**
-                 * @see Conjoon_Config_Exception
-                 */
-                require_once 'Conjoon/Config/Exception.php';
-                throw new Conjoon_Config_Exception(
-                    "Section '$section' cannot be found in $filename"
-                );
-            }
-            Zend_Config::__construct(
-                $this->_processExtends($preProcessedArray, $section), $allowModifications
-            );
-        }
-
-        $this->_loadedSection = $section;
+        return $iniArray;
     }
 
 }
