40a3bd679dc0adab5cd25d453448b44937126e6f
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2009-09-29 21:20:12 +0000

    - fixed: changed "statusUpdate()" implementation due to changes in Zend Framework 1.9.3PL1

--- src/corelib/php/library/Conjoon/Service/Twitter.php
+++ src/corelib/php/library/Conjoon/Service/Twitter.php
@@ -22,11 +22,6 @@ require_once 'Zend/Service/Twitter.php';
  * This class main purpose is to send a source id "conjoon" with any status update,
  * thus notifying the Twitter service that the update was done using conjoon.
  *
- * It does also override "favoriteCreate", "favoriteDestroy" and "statusDestroy",
- * since in ZF 1.7.8 the status ids would be casted to integer, which must not happen
- * (see #http://www.twitpocalypse.com/)
- *
- *
  * @author Thorsten Suckow-Homberg <ts@siteartwork.de>
  * @author The Zend Framework Team
  */
@@ -50,72 +45,23 @@ class Conjoon_Service_Twitter extends Zend_Service_Twitter {
     {
         $this->_init();
         $path = '/statuses/update.xml';
-        $len  = strlen($status);
-        //if ($len > 140) {
-       //     include_once 'Zend/Service/Twitter/Exception.php';
-        //    throw new Zend_Service_Twitter_Exception('Status must be no more than 140 characters in length');
-        //} else
-        if (0 == $len) {
+        $len = iconv_strlen(htmlspecialchars($status, ENT_QUOTES, 'UTF-8'), 'UTF-8');
+        if ($len > self::STATUS_MAX_CHARACTERS) {
+            include_once 'Zend/Service/Twitter/Exception.php';
+            throw new Zend_Service_Twitter_Exception('Status must be no more than '. self::STATUS_MAX_CHARACTERS .' characters in length');
+        } elseif (0 == $len) {
             include_once 'Zend/Service/Twitter/Exception.php';
             throw new Zend_Service_Twitter_Exception('Status must contain at least one character');
         }
-
         $data = array(
             'status' => $status,
             'source' => 'conjoon'
         );
-
-        if(is_numeric($in_reply_to_status_id) && !empty($in_reply_to_status_id)) {
+        if (is_numeric($in_reply_to_status_id) && ! empty($in_reply_to_status_id)) {
             $data['in_reply_to_status_id'] = $in_reply_to_status_id;
         }
-
         //$this->status = $status;
-        $response = $this->restPost($path, $data);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-
-    /**
-     * Remove a favorite
-     *
-     * @param  float $id Status ID you want to de-list as a favorite
-     * @return Zend_Rest_Client_Result
-     */
-    public function favoriteDestroy($id)
-    {
-        $this->_init();
-        $path = '/favorites/destroy/' . (float)$id . '.xml';
-
-        $response = $this->restPost($path);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-
-    /**
-     * Mark a status as a favorite
-     *
-     * @param  float $id Status ID you want to mark as a favorite
-     * @return Zend_Rest_Client_Result
-     */
-    public function favoriteCreate($id)
-    {
-        $this->_init();
-        $path = '/favorites/create/' .  (float)$id . '.xml';
-
-        $response = $this->restPost($path);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-
-    /**
-     * Destroy a status message
-     *
-     * @param  float $id ID of status to destroy
-     * @return Zend_Rest_Client_Result
-     */
-    public function statusDestroy($id)
-    {
-        $this->_init();
-        $path = '/statuses/destroy/' . (float)$id . '.xml';
-
-        $response = $this->restPost($path);
+        $response = $this->_post($path, $data);
         return new Zend_Rest_Client_Result($response->getBody());
     }
 
