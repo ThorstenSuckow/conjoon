5a7e35f21b18ea800741bc7c5ab3f28b2cf97d52
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2009-10-01 14:21:19 +0000

    - enhancement: added action helper "registry access" for easier access to the registry

---
+++ src/corelib/php/library/Conjoon/Controller/Action/Helper/RegistryAccess.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * conjoon
+ * (c) 2002-2009 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+/**
+ * @see Zend_Controller_Action_Helper_Abstract
+ */
+require_once 'Zend/Controller/Action/Helper/Abstract.php';
+
+/**
+ *
+ * @uses       Zend_Controller_Action_Helper_Abstract
+ * @category   Conjoon
+ * @package    Conjoon_Controller
+ * @subpackage Conjoon_Controller_Action_Helper
+ */
+class Conjoon_Controller_Action_Helper_RegistryAccess extends Zend_Controller_Action_Helper_Abstract
+{
+    /**
+     * @var Conjoon_Modules_Default_User $user
+     */
+    protected $_user = null;
+
+
+    /**
+     * Returns the user object as stored in the registry.
+     *
+     * @return Conjoon_Modules_Default_User
+     */
+    public function getUser()
+    {
+        if (!$this->_user) {
+            /**
+             * @see Conjoon_Keys
+             */
+            require_once 'Conjoon/Keys.php';
+
+            $this->_user = Zend_Registry::get(
+                Conjoon_Keys::REGISTRY_AUTH_OBJECT
+            )->getIdentity();
+        }
+
+        return $this->_user;
+    }
+
+    /**
+     * Returns the id of the user stored in the registry.
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->getUser()->getId();
+    }
+
+    /**
+     *
+     * @return Conjoon_Controller_Action_Helper_RegistryAccess
+     */
+    public function direct()
+    {
+        return $this;
+    }
+
+}
