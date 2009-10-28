2cc88be5b26278a262a6eedccba5fcfd121e6ab1
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2009-10-28 23:28:57 +0000

    - enhancement: removed workaround for http://framework.zend.com/issues/browse/ZF-8032 which became obsolete with Zend Framework 1.9.4

--- src/corelib/php/library/Conjoon/Service/Twitter.php
+++ src/corelib/php/library/Conjoon/Service/Twitter.php
@@ -33,8 +33,8 @@ class Conjoon_Service_Twitter extends Zend_Service_Twitter {
      * This implementation will send a sourceId "conjoon" to the Twitter service,
      * notifying that the corresponding update was done using conjoon.
      * This implementation will also skipp checking if the requested update is
-     * longer than 140 bytes, as this will be done by the Twitter service itself, truncating
-     * the status message if needed.
+     * longer than STATUS_MAX_CHARACTERS bytes, as this will be done by the Twitter
+     * service itself, truncating the status message if needed.
      *
      * @param  string $status
      * @param  int $in_reply_to_status_id
@@ -62,102 +62,6 @@ class Conjoon_Service_Twitter extends Zend_Service_Twitter {
         }
         //$this->status = $status;
         $response = $this->_post($path, $data);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-
-
-// -------- overrides for http://framework.zend.com/issues/browse/ZF-8032
-
-    /**
-     * Show extended information on a user.
-     *
-     * This override is added due to http://framework.zend.com/issues/browse/ZF-8032
-     *
-     * @param  int|string $id User ID or name
-     * @throws Zend_Http_Client_Exception if HTTP request fails or times out
-     * @return Zend_Rest_Client_Result
-     */
-    public function userShow ($id)
-    {
-        $this->_init();
-
-        if (is_numeric($id)) {
-            $id = $this->_validInteger($id);
-        } else {
-            $id = $this->_validateScreenName($id);
-        }
-
-        $path = '/users/show/' . $id . '.xml';
-        $response = $this->_get($path);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-    /**
-     * Create friendship
-     *
-     * This override is added due to http://framework.zend.com/issues/browse/ZF-8032
-     *
-     * @param  int|string $id User ID or name of new friend
-     * @throws Zend_Http_Client_Exception if HTTP request fails or times out
-     * @return Zend_Rest_Client_Result
-     */
-    public function friendshipCreate ($id)
-    {
-        if (is_numeric($id)) {
-            $id = $this->_validInteger($id);
-        } else {
-            $id = $this->_validateScreenName($id);
-        }
-
-        $this->_init();
-        $path = '/friendships/create/' . $$id . '.xml';
-        $response = $this->_post($path);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-    /**
-     * Destroy friendship
-     *
-     * This override is added due to http://framework.zend.com/issues/browse/ZF-8032
-     *
-     * @param  int|string $id User ID or name of friend to remove
-     * @throws Zend_Http_Client_Exception if HTTP request fails or times out
-     * @return Zend_Rest_Client_Result
-     */
-    public function friendshipDestroy ($id)
-    {
-        $this->_init();
-
-        if (is_numeric($id)) {
-            $id = $this->_validInteger($id);
-        } else {
-            $id = $this->_validateScreenName($id);
-        }
-
-        $path = '/friendships/destroy/' . $id . '.xml';
-        $response = $this->_post($path);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-    /**
-     * Friendship exists
-     *
-     * This override is added due to http://framework.zend.com/issues/browse/ZF-8032
-     *
-     * @param int|string $id User ID or name of friend to see if they are your friend
-     * @throws Zend_Http_Client_Exception if HTTP request fails or times out
-     * @return Zend_Rest_Client_result
-     */
-    public function friendshipExists ($id)
-    {
-        $this->_init();
-
-        if (is_numeric($id)) {
-            $id = $this->_validInteger($id);
-        } else {
-            $id = $this->_validateScreenName($id);
-        }
-
-        $path = '/friendships/exists.xml';
-        $data = array('user_a' => $this->getUsername() , 'user_b' => $id);
-        $response = $this->_get($path, $data);
         return new Zend_Rest_Client_Result($response->getBody());
     }
 
