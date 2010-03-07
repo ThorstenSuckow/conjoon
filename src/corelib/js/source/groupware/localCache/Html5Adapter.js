4b42339b52ccb41ec2a675ecc687f3e7b0fe6af1
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2010-03-07 09:12:09 +0000

    - enhancement: [Application cache - client] added window.applicationCache events to Html5Adapter to properly set the clear flag on the server once an empty manifest file has been send to the client

--- src/corelib/js/source/groupware/localCache/Html5Adapter.js
+++ src/corelib/js/source/groupware/localCache/Html5Adapter.js
@@ -18,12 +18,69 @@ Ext.namespace('com.conjoon.groupware.localCache');
  * An concrete Application Cache implementation for use with
  * com.conjoon.cudgets.localCache.Api based on HTML5 specifications.
  *
+ * Possible events fired by the window.applicationCache object:
+ * Function onerror;
+ * Function onnoupdate;
+ * Function ondownloading;
+ * Function onprogress;
+ * Function onupdateready;
+ * Function oncached;
+ * Function onobsolete;
+ *
+ *
+ *
  * @author Thorsten Suckow-Homberg <ts@siteartwork.de>
  *
  * @class com.conjoon.groupware.localCache.Html5Adapter
  * @extends com.conjoon.cudgets.localCache.Adapter
  */
-com.conjoon.groupware.localCache.Html5Adapter = Ext.extend(com.conjoon.cudgets.localCache.Adapter, {
+com.conjoon.groupware.localCache.Html5Adapter = function() {
+
+    this.addEvents(
+        'error',
+        'noupdate',
+        'downloading',
+        'progress',
+        'updateready',
+        'cached',
+        'obsolete'
+    );
+
+    var appCache = window.applicationCache;
+
+    if (appCache) {
+        var EventManager = Ext.EventManager;
+
+        EventManager.on(appCache, 'error', function(e) {
+            this.fireEvent('error', this);
+        } , this);
+        EventManager.on(appCache, 'noupdate', function(e) {
+            this.fireEvent('noupdate', this);
+        } , this);
+        EventManager.on(appCache, 'downloading', function(e) {
+            this.fireEvent('downloading', this);
+        } , this);
+        EventManager.on(appCache, 'progress', function(e) {
+            this.fireEvent('error', this);
+        } , this);
+        EventManager.on(appCache, 'updateready', function(e) {
+            this.fireEvent('updateready', this);
+        } , this);
+        EventManager.on(appCache, 'progress', function(e) {
+            this.fireEvent('progress', this);
+        } , this);
+        EventManager.on(appCache, 'cached', function(e) {
+            this.fireEvent('cached', this);
+        } , this);
+        EventManager.on(appCache, 'obsolete', function(e) {
+            this.fireEvent('obsolete', this);
+        } , this);
+    }
+
+    com.conjoon.groupware.localCache.Html5Adapter.superclass.constructor.call(this);
+};
+
+Ext.extend(com.conjoon.groupware.localCache.Html5Adapter, com.conjoon.cudgets.localCache.Adapter, {
 
 
 
@@ -61,24 +118,24 @@ com.conjoon.groupware.localCache.Html5Adapter = Ext.extend(com.conjoon.cudgets.l
                 if (succ === null || succ === false) {
                     this.fireEvent('clearfailure', this);
                 } else {
+                    this.on('updateready', function() {
+
+                        com.conjoon.defaultProvider.applicationCache.setClearFlag(
+                            {clear : false},
+                            function(provider, response) {
+                                var succ = com.conjoon.groupware.ResponseInspector.isSuccess(response);
+                                if (succ === null || succ === false) {
+                                    this.fireEvent('clearfailure', this);
+                                } else {
+                                    this.fireEvent('clearsuccess', this);
+                                }
+                            },
+                            this
+                        )
+                    }, this, {single : true});
 
                     window.applicationCache.swapCache();
                     window.applicationCache.update();
-                    /**
-                     * @todo need listener to trigger setting flag to false
-                     */
-                    /*com.conjoon.defaultProvider.applicationCache.setClearFlag(
-                        {clear : false},
-                        function(provider, response) {
-                            var succ = com.conjoon.groupware.ResponseInspector.isSuccess(response);
-                            if (succ === null || succ === false) {
-                                this.fireEvent('clearfailure', this);
-                            } else {
-                                this.fireEvent('clearsuccess', this);
-                            }
-                        },
-                        this
-                    );*/
                 }
 
         }, this);
