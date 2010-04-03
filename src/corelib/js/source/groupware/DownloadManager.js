8fcea4feb345d720cf9b07d37f977d184cd335fa
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2010-04-03 12:23:28 +0000

    - enhancement: [General] DownloadManager now uses an instance of Ext.util.Obervable internally for handling events

--- src/corelib/js/source/groupware/DownloadManager.js
+++ src/corelib/js/source/groupware/DownloadManager.js
@@ -15,22 +15,94 @@
 Ext.namespace('com.conjoon.groupware');
 
 /**
+ * The DownloadManager allows for subscribing to the following download
+ * related events:
+ * success, error, failure, request, cancel
+ *
  *
  * @class com.conjoon.groupware.DownloadManager
  * @singleton
  */
 com.conjoon.groupware.DownloadManager = function() {
 
+    var _kernel = function(){
+        this.addEvents(
+            /**
+             * @event success
+             * Fired when a download was processed on the server side
+             * successfully
+             * @param {com.conjoon.cudgets.data.Download} download
+             * @param {String} type
+             * @param {Object} options
+             */
+            'success',
+            /**
+             * @event error
+             * Fired when a download failed
+             * @param {com.conjoon.cudgets.data.Download} download
+             * @param {String} type
+             * @param {Object} options
+             */
+            'error',
+            /**
+             * @event failure
+             * Fired when a download failed
+             * @param {com.conjoon.cudgets.data.Download} download
+             * @param {String} type
+             * @param {Object} options
+             */
+            'failure',
+            /**
+             * @event request
+             * Fired when a download is requested
+             * @param {com.conjoon.cudgets.data.Download} download
+             * @param {String} type
+             * @param {Object} options
+             */
+            'request',
+            /**
+             * @event cancel
+             * Fired when a download was canceled.
+             * @param {com.conjoon.cudgets.data.Download} download
+             * @param {String} type
+             * @param {Object} options
+             */
+            'cancel'
+        );
+
+        _kernel.superclass.constructor.call(this);
+    };
+
+    Ext.extend(_kernel, Ext.util.Observable, {});
+
+    var kernel = new _kernel();
+
     var downloads = [];
+
+    var events = ['cancel', 'failure', 'error', 'request', 'success'];
 
 
     var removeDownload = function(url) {
         delete downloads[url];
     };
 
+    var DownloadManager = com.conjoon.groupware.DownloadManager;
+
     return {
 
-        downloadEmailAttachment : function(id, key, name, htmlElement)
+        /**
+         * Downloads an email attachment.
+         * The options argument for the events fired from the DownloadManager
+         * related to this download type are the following:
+         * attachmentId, attachmentKey, attachmentName.
+         * The type available in the events fired is "emailAttachment".
+         *
+         * @param {Number} id
+         * @param {Mixed} key
+         * @param {String} name
+         *
+         */
+        downloadEmailAttachment : function(id, key, name)
         {
             var url = './groupware/email.item/download.attachment/id/'
                 + id
@@ -47,67 +119,31 @@ com.conjoon.groupware.DownloadManager = function() {
 
             downloads[url] = download;
 
-            var func = function() {
-                Ext.fly(htmlElement).removeClass('request');
-                removeDownload(url);
-            };
+            var func = null;
+            for (var i = 0, len = events.length; i < len; i++) {
+                func = function(download, eventName) {
+                    if (eventName != 'request') {
+                        removeDownload(url);
+                    }
+                    kernel.fireEvent(eventName, download, 'emailAttachment', {
+                        attachmentId : id, attachmentKey : key, attachmentName : name
+                    }, DownloadManager);
+                };
 
-            var Bus = Ext.ux.util.MessageBus;
-
-            download.on('success', function(){
-                func();
-                Bus.publish(
-                    'com.conjoon.groupware.DownloadManager.success', {
-                        type : 'emailAttachment',
-                        name : name,
-                        url  : url
-                    }
-                )
-            });
-            download.on('error', function() {
-                func();
-                Bus.publish(
-                    'com.conjoon.groupware.DownloadManager.error', {
-                        type : 'emailAttachment',
-                        name : name,
-                        url  : url,
-                        text : com.conjoon.Gettext.gettext("Could not download.")
-                    }
-                );
-            });
-            download.on('request',       function() {
-                Ext.fly(htmlElement).addClass('request');
-                Ext.ux.util.MessageBus.publish(
-                    'com.conjoon.groupware.DownloadManager.request', {
-                        type : 'emailAttachment',
-                        name : name,
-                        url  : url
-                    }
-                );
-            });
-            download.on('cancel', function() {
-                func();
-                Bus.publish(
-                    'com.conjoon.groupware.DownloadManager.cancel', {
-                        type : 'emailAttachment',
-                        name : name,
-                        url  : url
-                    }
-                );
-            });
-            download.on('failure', function() {
-                func();
-                Bus.publish(
-                    'com.conjoon.groupware.DownloadManager.failure', {
-                        type : 'emailAttachment',
-                        name : name,
-                        url  : url,
-                        text : com.conjoon.Gettext.gettext("Could not download.")
-                    }
-                );
-            });
+                download.on(events[i], func.createDelegate(DownloadManager, [download, events[i]]));
+            }
 
             download.start();
+        },
+
+        on : function(eventName, func, scope, options)
+        {
+            kernel.on(eventName, func, scope, options);
+        },
+
+        un : function(eventName, func, scope)
+        {
+            kernel.on(eventName, func, scope);
         }
 
     };
