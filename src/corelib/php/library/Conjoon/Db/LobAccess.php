04f70cadffb0733dc15f0de9931113232c9ebb5a
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2010-06-01 01:22:15 +0000

    [File module - backend]  - enhancement: added interface for DB table models which have to deal with lobs; added concrete implementation for lob access for the filesystem

---
+++ src/corelib/php/library/Conjoon/Db/LobAccess.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * conjoon
+ * (c) 2002-2010 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+/**
+ * An interface for objects that gain read/write access to LOBs.
+ *
+ *
+ * @author Thorsten Suckow-Homberg <ts@siteartwork.de>
+ */
+interface Conjoon_Db_LobAccess extends Conjoon_Data_LobAccess {
+
+}
