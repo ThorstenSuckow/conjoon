ec3da88c9b2ede09a00064e397d3a80b3e8e9b00
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2011-11-15 00:23:28 +0000

    [General backend]  - enhancement: added Conjoon_Filter_Exception/Conjoon_Filter_DateUtc along with PHPUnit tests (and related PHPUnit files) to the project, in preparation of converting - and storing - datetime values from their orig. timezone to UTC timezone values in the format "YYYY-MM-dd HH:mm:ss"  (see CN-396)

---
+++ src/corelib/php/library/Conjoon/Filter/Exception.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * conjoon
+ * (c) 2002-2010 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+
+/**
+ * @see Conjoon_Exception
+ */
+require_once 'Conjoon/Exception.php';
+
+
+/**
+ * @category   Conjoon
+ * @package    Conjoon_Filter
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class Conjoon_Filter_Exception extends Conjoon_Exception
+{}
