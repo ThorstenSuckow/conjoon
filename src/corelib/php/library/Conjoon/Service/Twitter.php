a506c81f36126db1e52b0be3cc00ae90c37687fd
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2012-10-28 00:02:23 +0000

    [Twitter module - backend] - enhancement: removed override for "statusUpdate()", since recent import from Zend Framework 1.12 fixed the issues with the path to status updates; also, sending a source parameter is deprecated due to app identification using OAuth (closes CN-461)

--- src/corelib/php/library/Conjoon/Service/Twitter.php
+++ src/corelib/php/library/Conjoon/Service/Twitter.php
@@ -27,44 +27,6 @@ require_once 'Zend/Service/Twitter.php';
  */
 class Conjoon_Service_Twitter extends Zend_Service_Twitter {
 
-
-    /**
-     * Update user's current status
-     * This implementation will send a sourceId "conjoon" to the Twitter service,
-     * notifying that the corresponding update was done using conjoon.
-     * This implementation will also skipp checking if the requested update is
-     * longer than STATUS_MAX_CHARACTERS bytes, as this will be done by the Twitter
-     * service itself, truncating the status message if needed.
-     *
-     * @param  string $status
-     * @param  int $in_reply_to_status_id
-     * @return Zend_Rest_Client_Result
-     * @throws Zend_Service_Twitter_Exception if message is too short or too long
-     */
-    public function statusUpdate($status, $in_reply_to_status_id = null)
-    {
-        $this->_init();
-        $path = '1/statuses/update.xml';
-        $len = iconv_strlen(htmlspecialchars($status, ENT_QUOTES, 'UTF-8'), 'UTF-8');
-        if ($len > self::STATUS_MAX_CHARACTERS) {
-            include_once 'Zend/Service/Twitter/Exception.php';
-            throw new Zend_Service_Twitter_Exception('Status must be no more than '. self::STATUS_MAX_CHARACTERS .' characters in length');
-        } elseif (0 == $len) {
-            include_once 'Zend/Service/Twitter/Exception.php';
-            throw new Zend_Service_Twitter_Exception('Status must contain at least one character');
-        }
-        $data = array(
-            'status' => $status,
-            'source' => 'conjoon'
-        );
-        if (is_numeric($in_reply_to_status_id) && ! empty($in_reply_to_status_id)) {
-            $data['in_reply_to_status_id'] = $in_reply_to_status_id;
-        }
-        //$this->status = $status;
-        $response = $this->_post($path, $data);
-        return new Zend_Rest_Client_Result($response->getBody());
-    }
-
     /**
      * @see #ZF-9215 - "page" parameter does not work, instead
      * use "cursor"
