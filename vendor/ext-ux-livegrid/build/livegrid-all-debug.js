df473edc1b4931c73c9f748c730c7a916708c013
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2012-10-28 23:13:19 +0000

    [Ext.ux.Livegrid] - fixed: fixed an issue with sorting data by bringing Ext.ux.Livegrid 0.6.1 into main dev line (closes CN-459)

--- vendor/ext-ux-livegrid/build/livegrid-all-debug.js
+++ vendor/ext-ux-livegrid/build/livegrid-all-debug.js
@@ -3124,13 +3124,13 @@ Ext.extend(Ext.ux.grid.livegrid.Store, Ext.data.Store, {
     // private
     sortData : function(f, direction)
     {
-        direction = direction || 'ASC';
-        var st = this.fields.get(f).sortType;
-        var fn = function(r1, r2){
-            var v1 = st(r1.data[f]), v2 = st(r2.data[f]);
-            return v1 > v2 ? 1 : (v1 < v2 ? -1 : 0);
-        };
-        this.data.sort(direction, fn);
+        var snap = this.snapshot;
+
+        this.snapshot = false;
+
+        Ext.ux.grid.livegrid.Store.superclass.sortData.apply(this, arguments);
+
+        this.snapshot = snap;
     },
 
 
