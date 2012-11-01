c9319749ceeca4aab8e9c5c24a1810a603ce2aba
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2012-11-01 17:49:37 +0000

    [Ext.ux.grid.GridViewMenuPlugin] - fixed: fixed an issue that would allow to unselect all checkboxes in a grid's menu, regardless of if there is only one last remaining column visible by adding override to most likely erroneous Ext 3.4 implementation of Ext.grid.GridView.handleHdMenuClickDefault (closes CN-510)

--- src/corelib/js/source/Overrides.js
+++ src/corelib/js/source/Overrides.js
@@ -561,3 +561,36 @@ Ext.Viewport.prototype.initComponent = Ext.Viewport.prototype.initComponent.crea
         this._activeElement = document.activeElement;
     }
 );
+
+/*@REMOVE@*/
+if (Ext.version != '3.4.0') {
+    throw(
+        "Check override for " +
+        "Ext.grid.GridView.handleHdMenuClickDefault " +
+        "in Overrides.js"
+    );
+}
+/*@REMOVE@*/
+
+/**
+ * @bug 3.4 we override this since in Ext 3.4 "false" is not returned
+ * in case if the last remaining column is about to be "checked hidden"
+ */
+Ext.override(Ext.grid.GridView, {
+
+    handleHdMenuClickDefault: function(item) {
+        var colModel = this.cm,
+            itemId   = item.getItemId(),
+            index    = colModel.getIndexById(itemId.substr(4));
+
+        if (index != -1) {
+            if (item.checked && colModel.getColumnsBy(this.isHideableColumn, this).length <= 1) {
+                this.onDenyColumnHide();
+                // THIS! return false!!! Missing in 3.4
+                return false;
+            }
+            colModel.setHidden(index, item.checked);
+        }
+    }
+
+});
