4d438264a9e7d0dea20cd57d0fd97109ef94fb60
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2012-11-03 21:43:19 +0000

    [General] - enhancement: moved bug overrides to ext-bug-overrides.js (closes CN-528)

--- src/corelib/js/source/Overrides.js
+++ src/corelib/js/source/Overrides.js
@@ -565,39 +565,6 @@ Ext.Viewport.prototype.initComponent = Ext.Viewport.prototype.initComponent.crea
 /*@REMOVE@*/
 if (Ext.version != '3.4.0') {
     throw(
-        "Check override for " +
-        "Ext.grid.GridView.handleHdMenuClickDefault " +
-        "in Overrides.js"
-    );
-}
-/*@REMOVE@*/
-
-/**
- * @bug 3.4 we override this since in Ext 3.4 "false" is not returned
- * in case if the last remaining column is about to be "checked hidden"
- */
-Ext.override(Ext.grid.GridView, {
-
-    handleHdMenuClickDefault: function(item) {
-        var colModel = this.cm,
-            itemId   = item.getItemId(),
-            index    = colModel.getIndexById(itemId.substr(4));
-
-        if (index != -1) {
-            if (item.checked && colModel.getColumnsBy(this.isHideableColumn, this).length <= 1) {
-                this.onDenyColumnHide();
-                // THIS! return false!!! Missing in 3.4
-                return false;
-            }
-            colModel.setHidden(index, item.checked);
-        }
-    }
-
-});
-
-/*@REMOVE@*/
-if (Ext.version != '3.4.0') {
-    throw(
         "Check override for Ext.ProgressBar in Overrides.js"
     );
 }
