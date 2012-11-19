403c3c2297126a2b36112c9e76054242e5826ef3
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2012-11-19 21:37:15 +0000

    [General Backend] - enhancement: updated Conjoon_Exception to receive a third parameter pointing to a previous exception for exception chaining (closes CN-630)

--- src/corelib/php/library/Conjoon/Exception.php
+++ src/corelib/php/library/Conjoon/Exception.php
@@ -16,6 +16,9 @@
 /**
  * Exception for Conjoon namespace.
  *
+ * Some Code was taken from Zend Framework 1.12, specially the
+ * "previous exception" code.
+ *
  * @uses Exception
  * @package Conjoon
  *
@@ -23,5 +26,70 @@
  */
 class Conjoon_Exception extends Exception {
 
+    /**
+     * @var null|Exception
+     */
+    private $_previous = null;
+
+    /**
+     * Constructs a new exception.
+     *
+     * @param string $message
+     * @param int $code
+     * @param Exception|null $previous
+     */
+    public function __construct($msg = "", $code = 0, Exception $previous = null)
+    {
+        if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+            parent::__construct($msg, (int) $code);
+            $this->_previous = $previous;
+        } else {
+            parent::__construct($msg, (int) $code, $previous);
+        }
+    }
+
+    /**
+     * Overloading
+     *
+     * For PHP < 5.3.0, provides access to the getPrevious() method.
+     *
+     * @param  string $method
+     * @param  array $args
+     * @return mixed
+     */
+    public function __call($method, array $args)
+    {
+        if ('getprevious' == strtolower($method)) {
+            return $this->_getPrevious();
+        }
+        return null;
+    }
+
+    /**
+     * String representation of the exception
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+            if (null !== ($e = $this->getPrevious())) {
+                return $e->__toString()
+                    . "\n\nNext "
+                    . parent::__toString();
+            }
+        }
+        return parent::__toString();
+    }
+
+    /**
+     * Returns previous Exception
+     *
+     * @return Exception|null
+     */
+    protected function _getPrevious()
+    {
+        return $this->_previous;
+    }
 
 }
