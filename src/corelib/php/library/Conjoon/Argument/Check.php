6d643df567fb330c4a098685990a0ca969e18aca
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2013-11-26 21:28:53 +0000

    [conjoon API] - enhancement: legacy code for Conjoon_Argument_Check now inherits new implementation

--- src/corelib/php/library/Conjoon/Argument/Check.php
+++ src/corelib/php/library/Conjoon/Argument/Check.php
@@ -13,10 +13,16 @@
  * $URL$
  */
 
+
 /**
  * @see Conjoon_Argument_Exception
  */
 require_once 'Conjoon/Argument/Exception.php';
+
+/**
+ * @see \Conjoon\Argument\ArgumentCheck
+ */
+require_once 'Conjoon/Argument/ArgumentCheck.php';
 
 /**
  *
@@ -26,154 +32,7 @@ require_once 'Conjoon/Argument/Exception.php';
  *
  * @deprecated use Conjoon\Argument\ArgumentCheck
  */
-class Conjoon_Argument_Check {
+class Conjoon_Argument_Check extends \Conjoon\Argument\ArgumentCheck {
 
-    private function __construct(){}
-
-    private function __clone(){}
-
-    /**
-     *
-     *
-     * @throws Conjoon_Argument_Exception
-     */
-    public static function check(Array $config, Array &$data)
-    {
-
-        foreach ($config as $argumentName => $entityConfig) {
-
-            if (!array_key_exists($argumentName, $data)) {
-                throw new Conjoon_Argument_Exception(
-                    "\"$argumentName\" does not exist in data"
-                );
-            }
-
-            $allowEmpty = isset($entityConfig['allowEmpty'])
-                          ? $entityConfig['allowEmpty']
-                          : false;
-
-            $greaterThan = isset($entityConfig['greaterThan'])
-                           ? (int)(string)$entityConfig['greaterThan']
-                           : false;
-
-            switch ($entityConfig['type']) {
-
-                case 'instanceof':
-
-                    if (!$allowEmpty && !isset($data[$argumentName])) {
-                        throw new Conjoon_Argument_Exception(
-                            "\"$argumentName\" not set"
-                        );
-                    }
-
-                    $className = $entityConfig['class'];
-
-                    if (!($data[$argumentName] instanceof $className)) {
-                        throw new Conjoon_Argument_Exception(
-                            "\"$argumentName\" not instanceof " .
-                            $entityConfig['class']
-                        );
-                    }
-
-
-                    break;
-
-                case 'inArray':
-                    $values = &$entityConfig['values'];
-
-                    if (!isset($data[$argumentName])) {
-                        throw new Conjoon_Argument_Exception(
-                            "\"$argumentName\" not set"
-                        );
-                    }
-
-                    if (!in_array($data[$argumentName], $values)) {
-                        throw new Conjoon_Argument_Exception(
-                            "\"".$data[$argumentName]."\" not in list of [".
-                                implode(', ', $values)."]"
-                        );
-                    }
-
-                    break;
-
-                case 'isset':
-                    if (!isset($data[$argumentName])) {
-                        throw new Conjoon_Argument_Exception(
-                            "\"$argumentName\" not set"
-                        );
-                    }
-                    break;
-
-
-                case 'bool':
-                    if (isset($data[$argumentName])) {
-                        $data[$argumentName] = (bool)$data[$argumentName];
-                    } else if ($allowEmpty === false) {
-                        throw new Conjoon_Argument_Exception(
-                            "no argument provided for $argumentName"
-                        );
-                    }
-                break;
-
-                case 'string':
-
-                    if (is_array($data[$argumentName])
-                        || is_object($data[$argumentName])) {
-                        throw new Conjoon_Argument_Exception(
-                            "Array or object passed for $argumentName - "
-                            . (is_array($data[$argumentName])
-                            ? 'array'
-                            :'object')
-                        );
-                    }
-
-                    $val = trim((string)$data[$argumentName]);
-                    $org = $data[$argumentName];
-
-                    if ($allowEmpty === false && $val === "") {
-                        throw new Conjoon_Argument_Exception(
-                            "empty value provided for $argumentName"
-                        );
-                    }
-
-                    $data[$argumentName] = $val;
-
-                break;
-
-                case 'int':
-
-                    if (is_array($data[$argumentName])
-                        || is_object($data[$argumentName])) {
-                        throw new Conjoon_Argument_Exception(
-                            "Array or object passed for $argumentName - "
-                                . (is_array($data[$argumentName])
-                                ? 'array'
-                                :'object')
-                        );
-                    }
-
-                    $val = (int)trim((string)$data[$argumentName]);
-                    $org = $data[$argumentName];
-
-                    if ($allowEmpty === false
-                        && ($org === null || $org === "")) {
-                        throw new Conjoon_Argument_Exception(
-                            "empty value provided for $argumentName"
-                        );
-                    }
-
-                    if ($greaterThan !== false && $val <= $greaterThan) {
-                        throw new Conjoon_Argument_Exception(
-                            "value \"$argumentName\" must be > "
-                            . $greaterThan .", was $val"
-                        );
-                    }
-
-                    $data[$argumentName] = $val;
-
-                break;
-            }
-        }
-    }
 
 }
