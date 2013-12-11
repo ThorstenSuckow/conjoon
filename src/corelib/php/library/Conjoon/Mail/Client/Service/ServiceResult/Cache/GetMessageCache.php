4cf07ab783f79659608711e7b1fd9e7079535564
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2013-12-11 18:13:11 +0000

    [conjoon API] - enhancement: added interface for GetMessageServiceResult cache classes (see CN-787)

---
+++ src/corelib/php/library/Conjoon/Mail/Client/Service/ServiceResult/Cache/GetMessageCache.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * conjoon
+ * (c) 2002-2012 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+
+namespace Conjoon\Mail\Client\Service\ServiceResult\Cache;
+
+/**
+ * @see \Conjoon\Data\Cache\Cacheable
+ */
+require_once 'Conjoon/Data/Cache/Cacheable.php';
+
+
+/**
+ * Interface for caching GetMessageServiceResults
+ *
+ * @package Conjoon
+ * @category Conjoon\Service
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+interface GetMessageCache extends \Conjoon\Data\Cache\Cacheable {
+
+}
---
+++ src/corelib/php/tests/Conjoon/Mail/Client/Service/ServiceResult/Cache/GetMessageCacheTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * conjoon
+ * (c) 2002-2012 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+
+namespace Conjoon\Mail\Client\Service\ServiceResult\Cache;
+
+/**
+ * @see GetMessageCacheKey
+ */
+require_once 'Conjoon/Mail/Client/Service/ServiceResult/Cache/GetMessageCache.php';
+
+/**
+ * @category   Conjoon
+ * @package    Conjoon_Service
+ * @subpackage UnitTests
+ * @group      Conjoon_Service
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class GetMessageCacheTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Ensures everything works as expected
+     */
+    public function testDummy() {
+
+    }
+
+}
