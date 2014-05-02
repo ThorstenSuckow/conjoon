e3500c798f9082328bf4b6c258981272f378ef4f
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2014-05-02 11:43:51 +0000

    [install wizard] - enhancement: added doctrine cache settings to wizard, whereas default values depend on config.ini.php, rather than collected data in installation-info (closes CN-831)

--- src/www/htdocs/install/lib_settings.php
+++ src/www/htdocs/install/lib_settings.php
@@ -37,25 +37,75 @@ $DOCTRINE_CACHE_TYPES = array(
 
 $DOCTRINE_CACHE_EXTENSIONS = array('apc', 'memcache', 'memcached', 'file');
 
-function cacheSetup_getCacheDir($key, $value = null)
+
+/**
+ * Helps to retrieve a default value for a specific setting.
+ * The default value is looked up in the config_ini, then installation_info, then
+ * setup_ini.
+ *
+ *
+ */
+function cacheSetup_getConfigurationDefaultValue($key, $section, $config = array())
 {
-    if (isset($_SESSION['installation_info']['application.' . $key])
-    && !empty($_SESSION['installation_info']['application.' . $key])) {
-        return $_SESSION['installation_info']['application.' . $key];
+    $cacheSetup =& $_SESSION['setup_ini'][$section];
+
+    $allowEmpty = isset($config['allowEmpty']) ? $config['allowEmpty'] : true;
+
+    // check first of value is available in config
+    if (isset($_SESSION['config_info']) &&
+        is_array($_SESSION['config_info']) &&
+        isset($_SESSION['config_info'][$section]) &&
+        array_key_exists($key, $_SESSION['config_info'][$section])) {
+        if ($allowEmpty || (!$allowEmpty && $_SESSION['config_info'][$section][$key])) {
+            return $_SESSION['config_info'][$section][$key];
+        }
     }
 
-    $cacheSetup =& $_SESSION['setup_ini']['application'];
+    // check if value is available in installation_info
+    if (array_key_exists($section . '.' . $key, $_SESSION['installation_info'])
+        && !empty($_SESSION['installation_info']['application.' . $key])) {
+        if ($allowEmpty || (!$allowEmpty && $_SESSION['installation_info']['application.' . $key])) {
+            return $_SESSION['installation_info']['application.' . $key];
+        }
+    }
 
-    if (strpos($key, 'cache_dir') !== false ||
-        strpos($key, '.dir') !== false) {
-        return rtrim($_SESSION['app_path'], '/')
-               . '/'
-               . rtrim($_SESSION['setup_ini']['app_path']['folder'], '/')
-               . '/'
-               . $cacheSetup[$key];
-    } else {
+    // check if value is available in setup ini
+    if (isset($_SESSION['setup_ini'][$section]) &&
+        array_key_exists($key, $_SESSION['setup_ini'][$section])) {
+        return $_SESSION['setup_ini'][$section][$key];
+    }
+
+    return null;
+}
+
+
+
+/**
+ * Helper function to assign setup default values for directory path values.
+ *
+ * @param $key
+ * @param null $value
+ * @return null|string
+ */
+function cacheSetup_assembleDir($key, $section, $value = null)
+{
+    $cacheSetup =& $_SESSION['setup_ini'][$section];
+
+    if (!$value) {
+        $value = $cacheSetup[$key];
+    }
+
+    if ($value && strpos($value, '/') === 0) {
         return $value;
     }
+
+    // fall back to default value from setup.ini, prepending app_path
+    return rtrim($_SESSION['app_path'], '/')
+        . '/'
+        . rtrim($_SESSION['setup_ini']['app_path']['folder'], '/')
+        . '/'
+        . $value;
+
 }
 
 
@@ -66,27 +116,33 @@ if (isset($_SESSION['application'])) {
 } else {
 
     $getMyKeys = array(
-        'htmlpurifier.preload_all',
-        'htmlpurifier.use_cache',
-        'doctrine.cache.enabled',
-        'doctrine.cache.query_cache.enabled',
-        'doctrine.cache.query_cache.type',
-        'doctrine.cache.query_cache.dir',
-        'doctrine.cache.metadata_cache.enabled',
-        'doctrine.cache.metadata_cache.type',
-        'doctrine.cache.metadata_cache.dir',
+        'htmlpurifier.preload_all' => array(),
+        'htmlpurifier.use_cache' => array(),
+        'htmlpurifier.cache_dir' => array(),
+        'doctrine.cache.enabled' => array(),
+        'doctrine.cache.query_cache.enabled' => array(),
+        'doctrine.cache.query_cache.type' => array('allowEmpty' => false),
+        'doctrine.cache.query_cache.dir' => array(),
+        'doctrine.cache.metadata_cache.enabled' => array(),
+        'doctrine.cache.metadata_cache.type' => array('allowEmpty' => false),
+        'doctrine.cache.metadata_cache.dir' => array(),
     );
 
-    foreach ($getMyKeys as $heresYourKey) {
-        $LIB_SETTINGS[$heresYourKey] = isset($_SESSION['installation_info']['application.' . $heresYourKey])
-            && !empty($_SESSION['installation_info']['application.' . $heresYourKey])
-            ? $_SESSION['installation_info']['application.' . $heresYourKey]
-            : $_SESSION['setup_ini']['application'][$heresYourKey];
+    foreach ($getMyKeys as $heresYourKey => $heresYourValue) {
+
+        // gather default value!
+        $LIB_SETTINGS[$heresYourKey] = cacheSetup_getConfigurationDefaultValue(
+            $heresYourKey, 'application', $getMyKeys[$heresYourKey]
+        );
+
+        // adjust the value if necessary
+        if (strpos($heresYourKey, 'cache_dir') !== false ||
+            strpos($heresYourKey, '.dir') !== false) {
+            $LIB_SETTINGS[$heresYourKey] = cacheSetup_assembleDir($heresYourKey, 'application', $LIB_SETTINGS[$heresYourKey]);
+        }
+
     }
 
-    foreach ($applicationSetup as $key => $value) {
-        $LIB_SETTINGS[$key] = cacheSetup_getCacheDir($key, $value);
-    }
 }
 
 if (isset($_POST['lib_settings_post']) && $_POST['lib_settings_post'] == "1") {
