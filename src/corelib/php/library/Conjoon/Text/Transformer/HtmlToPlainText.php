914035b296005278454b34c0c5d1211b061642fe
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2015-02-03 19:16:20 +0000

    [Email module - backend] - enhancement: added HtmlToPlainText to API (see CN-909)

---
+++ src/corelib/php/library/Conjoon/Text/Transformer/HtmlToPlainText.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * conjoon
+ * (c) 2007-2015 conjoon.org
+ * licensing@conjoon.org
+ *
+ * conjoon
+ * Copyright (C) 2014 Thorsten Suckow-Homberg/conjoon.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+namespace Conjoon\Text\Transformer;
+
+/**
+ * @see Conjoon_Text_Transformer
+ */
+require_once 'Conjoon/Text/Transformer.php';
+
+/**
+ * @see \Conjoon\Argument\ArgumentCheck
+ */
+require_once 'Conjoon/Argument/ArgumentCheck.php';
+
+use \Conjoon\Argument\ArgumentCheck as ArgumentCheck;
+
+/**
+ * Returns a plain text version of a html text.
+ * Blockquotes will be transformed to ascii quotes (leading ">")
+ * Some line feeds and whitespaces will be included to format the text as
+ * much as possible to keep it readable.
+ *
+ * Example:
+ *
+ * Input:
+ * ======
+ * <pre>--------Original Message:-----------
+ * <table><tbody>
+ *  <tr><td>Subject:    </td> <td>             Test</td></tr>
+ *  <tr><td> Date: </td> <td>03.02.2015</td></tr>
+ * </tbody>
+ *
+ * </table>
+ * <blockquote>Text which is in here does not <br> conform to </blockquote>
+ * </pre>
+ *
+ * Output:
+ * =======
+ * --------Original Message:-----------\n\n Subject: Test\n Date: 03.02.2015\n\n>Text which is in here does not\n>conform to\n
+ *
+ *
+ * @uses Conjoon_Text_Transformer
+ * @category   Text
+ * @package    Conjoon_Text
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class HtmlToPlainText extends \Conjoon_Text_Transformer {
+
+
+    /**
+     * @type TagWhiteSpaceRemover
+     */
+    protected $tagWhiteSpaceRemover;
+
+    /**
+     * @type MultipleWhiteSpaceRemover
+     */
+    protected $multipleWhiteSpaceRemover;
+
+    /**
+     * @type NormalizeLineFeeds
+     */
+    protected $normalizeLineFeedsTransformer;
+
+    /**
+     * @type BlockquoteToQuote
+     */
+    protected $blockquoteToQuoteTransformer;
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct(Array $options = array()) {
+
+        ArgumentCheck::check(array(
+            'blockquoteToQuote' => array(
+                'type'       => 'instanceof',
+                'class'      => '\Conjoon\Text\Transformer\BlockquoteToQuote',
+                'allowEmpty' => false
+            ),
+            'normalizeLineFeeds' => array(
+                'type'       => 'instanceof',
+                'class'      => '\Conjoon\Text\Transformer\NormalizeLineFeeds',
+                'allowEmpty' => false
+            ),
+            'multipleWhiteSpaceRemover' => array(
+                'type'       => 'instanceof',
+                'class'      => '\Conjoon\Text\Transformer\MultipleWhiteSpaceRemover',
+                'allowEmpty' => false
+            ),
+            'tagWhiteSpaceRemover' => array(
+                'type'       => 'instanceof',
+                'class'      => '\Conjoon\Text\Transformer\TagWhiteSpaceRemover',
+                'allowEmpty' => false
+            )
+        ), $options);
+
+
+        $this->blockquoteToQuoteTransformer  = $options['blockquoteToQuote'];
+        $this->normalizeLineFeedsTransformer = $options['normalizeLineFeeds'];
+        $this->multipleWhiteSpaceRemover     = $options['multipleWhiteSpaceRemover'];
+        $this->tagWhiteSpaceRemover          = $options['tagWhiteSpaceRemover'];
+    }
+
+    /**
+     * @inherit Conjoon_Text_Transformer::transform
+     */
+    public function transform($input)
+    {
+        $data = array('input' => $input);
+
+        ArgumentCheck::check(array(
+            'input' => array(
+                'type'       => 'string',
+                'allowEmpty' => true
+             )
+        ), $data);
+
+        $value = $data['input'];
+
+        $value = $this->blockquoteToQuoteTransformer->transform(
+            str_replace('&nbsp;', '',
+                str_replace(
+                    array("<br>", "<br/>", "<br />", "<BR>", "<BR/>", "<BR />"),
+                    "\n",
+                    // remove opening/closing tag followed by whitespace
+                    str_replace(array('> ', ' <'), array('>', '<'),
+                        $this->multipleWhiteSpaceRemover->transform(
+                            $this->tagWhiteSpaceRemover->transform(
+                                $this->normalizeLineFeedsTransformer->transform(
+                                    $value
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        );
+
+        // add some linebreaks to a few block elements
+        $value = str_replace(
+            array('<tr>', '<TR>', '<table>', '<TABLE>', '</table>', '</TABLE>'),
+            "\n",
+            $value
+        );
+
+        // add some whitespaces to cell elements and such to make sure
+        // formatting is nice
+        $value = str_replace(
+            array('<td>', '<TD>'),
+            "<td> ",
+            $value
+        );
+
+        // now strip all tags!
+        $value = strip_tags($value);
+
+        // ...and convert all special html entities back!
+        return htmlspecialchars_decode($value);
+    }
+
+}
---
+++ src/corelib/php/tests/Conjoon/Text/Transformer/HtmlToPlainTextTest.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * conjoon
+ * (c) 2007-2015 conjoon.org
+ * licensing@conjoon.org
+ *
+ * conjoon
+ * Copyright (C) 2014 Thorsten Suckow-Homberg/conjoon.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+namespace Conjoon\Text\Transformer;
+
+/**
+ * @see \Conjoon\Text\Transformer\HtmlToPlainText
+ */
+require_once 'Conjoon/Text/Transformer/HtmlToPlainText.php';
+
+/**
+ * @see \Conjoon\Text\Transformer\BlockquoteToQuote
+ */
+require_once 'Conjoon/Text/Transformer/BlockquoteToQuote.php';
+
+/**
+ * @see \Conjoon\Text\Transformer\NormalizeLineFeeds
+ */
+require_once 'Conjoon/Text/Transformer/NormalizeLineFeeds.php';
+
+/**
+ * @see \Conjoon\Text\Transformer\MultipleWhiteSpaceRemover
+ */
+require_once 'Conjoon/Text/Transformer/MultipleWhiteSpaceRemover.php';
+
+/**
+ * @see \Conjoon\Text\Transformer\TagWhiteSpaceRemover
+ */
+require_once 'Conjoon/Text/Transformer/TagWhiteSpaceRemover.php';
+
+
+/**
+ * @package    Conjoon/Tests
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class HtmlToPlainTextTest extends \PHPUnit_Framework_TestCase {
+
+    protected $_transformer = null;
+
+    protected $_inputs = array();
+
+    /**
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->_transformer = new HtmlToPlainText(array(
+            'blockquoteToQuote'
+                => new \Conjoon\Text\Transformer\BlockquoteToQuote(),
+            'normalizeLineFeeds'
+                => new \Conjoon\Text\Transformer\NormalizeLineFeeds(),
+            'multipleWhiteSpaceRemover'
+                => new  \Conjoon\Text\Transformer\MultipleWhiteSpaceRemover(),
+            'tagWhiteSpaceRemover'
+                => new \Conjoon\Text\Transformer\TagWhiteSpaceRemover()
+        ));
+
+        $this->_inputs = array(
+            "<pre>--------Original Message:-----------
+             <table><tbody>
+              <tr><td>Subject:    </td> <td>             Test</td></tr>
+               <tr><td> Date: </td> <td>03.02.2015</td></tr>
+              </tbody>
+
+              </table>
+              <blockquote>Text which is in here does not <br> conform to </blockquote>
+              </pre>"
+            => "--------Original Message:-----------\n\n Subject: Test\n Date: 03.02.2015\n\n>Text which is in here does not\n>conform to\n"
+        );
+
+    }
+
+    /**
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+
+    }
+
+// +---------------------------------------------------------------------------
+// | Tests
+// +---------------------------------------------------------------------------
+
+    /**
+     * Ensure everything works as expected.
+     *
+     */
+    public function testTransform()
+    {
+
+        foreach ($this->_inputs as $input => $output) {
+            $this->assertEquals($output, $this->_transformer->transform($input));
+        }
+    }
+
+    /**
+     * @expectedException \Conjoon\Argument\InvalidArgumentException
+     */
+    public function testInvalidArgument()
+    {
+        $this->_transformer->transform(array());
+    }
+
+
+}
