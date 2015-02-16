1707ad8cf792e44feb38379aedd921abbeb1d675
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2015-02-16 21:26:44 +0000

    [conjoon API] - enhancement: added security exception if adding folder to target folder is forbidden (closes CN-932)

---
+++ src/corelib/php/library/Conjoon/Mail/Client/Security/FolderAddException.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * conjoon
+ * (c) 2007-2015 conjoon.org
+ * licensing@conjoon.org
+ *
+ * conjoon
+ * Copyright (C) 2014 Thorsten Suckow-Homberg/conjoon.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+namespace Conjoon\Mail\Client\Security;
+
+/**
+ * @see SecurityException
+ */
+require_once 'Conjoon/Mail/Client/Security/SecurityException.php';
+
+
+/**
+ * Exception to be thrown whenever a folder is to be added to another folder,
+ * but the operation is not allowed.
+ *
+ * @category   Conjoon
+ * @package    Exception
+ *
+ * @uses SecurityException
+ */
+class FolderAddException extends SecurityException {
+
+}
