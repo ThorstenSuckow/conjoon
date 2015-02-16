5a179ae888958f0b1c3876fd92a770cd75419bfd
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2015-02-16 21:30:49 +0000

    [conjoon API] - enhancement: added exception which is to be thrown when folders should be added to a target folder, but this folder does not allow for child folders (closes CN-933)

---
+++ src/corelib/php/tests/Conjoon/Mail/Client/Folder/NoChildFoldersAllowedExceptionTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * conjoon
+ * (c) 2007-2015 conjoon.org
+ * licensing@conjoon.org
+ *
+ * conjoon
+ * Copyright (C) 2014 Thorsten Suckow-Homberg/conjoon.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+namespace Conjoon\Mail\Client\Folder;
+
+/**
+ * @see Conjoon\Mail\Client\Folder\NoChildFoldersAllowedException
+ */
+require_once 'Conjoon/Mail/Client/Folder/NoChildFoldersAllowedException.php';
+
+/** @category   Conjoon
+ * @package    Conjoon_Mail
+ * @subpackage UnitTests
+ * @group      Conjoon_Mail
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class NoChildFoldersAllowedExceptionTest extends \PHPUnit_Framework_TestCase {
+
+
+    /**
+     * Ensures everything works as expected
+     *
+     * @expectedException \Conjoon\Mail\Client\Folder\NoChildFoldersAllowedException
+     *
+     * @return void
+     */
+    public function testException()
+    {
+        throw new NoChildFoldersAllowedException();
+    }
+
+}
