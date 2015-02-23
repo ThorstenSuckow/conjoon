060f1330a72c301f76951018423d95e66f10a8a8
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2015-02-23 21:05:10 +0000

    [conjoon API] - enhancement: added access class for folder type information of data structure (closes CN-945)

---
+++ src/corelib/php/tests/Conjoon/Data/Meta/Mail/FolderTypesTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * conjoon
+ * (c) 2007-2015 conjoon.org
+ * licensing@conjoon.org
+ *
+ * conjoon
+ * Copyright (C) 2014 Thorsten Suckow-Homberg/conjoon.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+
+namespace Conjoon\Data\Meta\Mail;
+
+/**
+ * @see \Conjoon\Data\Meta\Mail\FolderTypes
+ */
+require_once 'Conjoon/Data/Meta/Mail/FolderTypes.php';
+
+/**
+ * @package    Conjoon/Tests
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class FolderTypesTest extends \PHPUnit_Framework_TestCase {
+
+
+    /**
+     * Ensures everything works as expected
+     */
+    public function testOk()
+    {
+        // intentionally left empty
+    }
+
+}
