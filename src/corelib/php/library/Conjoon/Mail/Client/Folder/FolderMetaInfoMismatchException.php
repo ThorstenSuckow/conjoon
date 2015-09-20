eb1cabea28b6a67d7873ab61ec8ba8bb9c633458
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2015-09-20 20:10:29 +0000

    [conjoon API] - enhancement: added FolderMetaInfoMismatchException (see CN-956)

---
+++ src/corelib/php/library/Conjoon/Mail/Client/Folder/FolderMetaInfoMismatchException.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * conjoon
+ * (c) 2007-2015 conjoon.org
+ * licensing@conjoon.org
+ *
+ * conjoon
+ * Copyright (C) 2014 Thorsten Suckow-Homberg/conjoon.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+
+namespace Conjoon\Mail\Client\Folder;
+
+/**
+ * @see Conjoon\Mail\Client\Folder\FolderException
+ */
+require_once 'Conjoon/Mail/Client/Folder/FolderException.php';
+
+
+/**
+ * Exception to be thrown whenever an operation on two folders is to be executed
+ * which do not share the same exact meta information.
+ *
+ * @category   Conjoon
+ * @package    Exception
+ *
+ * @uses Conjoon\Mail\Client\Folder\FolderException
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class FolderMetaInfoMismatchException
+    extends FolderException {
+
+}
+
