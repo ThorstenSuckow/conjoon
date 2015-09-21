9325d7964b3f6f0d5b01b059f94aa766a61f0a1f
T. Suckow <ThorstenSuckow@users.noreply.github.com>
2015-09-21 18:18:50 +0000

    [conjoon API] - enhancement: added "AccountBasicService" to Mail\Client\Account namespace to provide basic services related to mail accounts (see CN-959)

---
+++ src/corelib/php/tests/Conjoon/Mail/Client/Account/AccountBasicServiceTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * conjoon
+ * (c) 2007-2015 conjoon.org
+ * licensing@conjoon.org
+ *
+ * conjoon
+ * Copyright (C) 2014 Thorsten Suckow-Homberg/conjoon.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+namespace Conjoon\Mail\Client\Account;
+
+/**
+ * @see Conjoon\Mail\Client\Account\AccountBasicService
+ */
+require_once 'Conjoon/Mail/Client/Account/AccountBasicService.php';
+
+/**
+ * @package Conjoon/Tests
+ *
+ * @author Thorsten Suckow-Homberg <tsuckow@conjoon.org>
+ */
+class AccountBasicServiceTest extends \PHPUnit_Framework_TestCase {
+
+
+    public function testDummy()
+    {
+
+    }
+
+}
